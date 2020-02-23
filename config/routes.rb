9a6e3ddfba8739bc5f5282a38cfc677ef4664a99
Kate Donaldson <kate@katelovescode.com>
2020-02-23 11:38:47 -0600

    Adds route constraint back in so this doesn't break the API responses

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
     end
   end
 
-  get '*path', to: 'application#fallback_index_html'
+  get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
+    !request.xhr? && request.format.html?
+  }
 end
 
 # rubocop:disable Metrics/LineLength
