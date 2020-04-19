13380d90d794b8e665fa229e9e905c93da28a2f1
Arun Kumar Mohan <arunmohandm@gmail.com>
2020-04-19 16:26:14 -0500

    feat: Setup Sentry to track error events

---
+++ client/src/ErrorBoundary/index.js
@@ -0,0 +1,1 @@
+export { ErrorBoundary as default } from './ErrorBoundary'
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as Sentry from '@sentry/browser'
 
-console.log('loading the app')
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN
+  })
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
