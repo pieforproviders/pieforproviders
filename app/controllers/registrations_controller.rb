1c47603ae76dc2b389b60bf98b98d3356618c099
Kate Donaldson <kate@katelovescode.com>
2020-04-28 22:08:43 -0500

    Updates params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,4 +10,16 @@ class RegistrationsController < Devise::RegistrationsController
     resource.save
     render_resource(resource)
   end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(
+      :active, :email, :full_name, :greeting_name,
+      :id, :language, :mobile, :opt_in_email,
+      :opt_in_phone, :opt_in_text, :organization, :password,
+      :password_confirmation, :phone, :service_agreement_accepted,
+      :slug, :timezone
+    )
+  end
 end
