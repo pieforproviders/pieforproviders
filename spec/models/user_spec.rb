46695d1d4689f68b6b90f997fdc871fb49da0416
Kate Donaldson <kate@katelovescode.com>
2020-05-02 16:34:33 -0500

    Fixes the user model spec to work with Devise's downcasing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:language) }
   it { should validate_presence_of(:organization) }
