247ecbc9d9513dbf48d20c8a6349e28f181f3119
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2020-07-07 21:07:18 -0500

    pull off image styles from css, use tailwind

--- client/src/_shared/AuthLayout.js
+++ client/src/_shared/AuthLayout.js
@@ -9,12 +9,16 @@ export function AuthLayout({ backgroundImageClass, rightColumnContent }) {
       <Col
         xs={{ span: 0 }}
         lg={{ span: 12 }}
-        className={`h-screen ${backgroundImageClass}`}
+        className={`h-screen block bg-no-repeat bg-cover ${backgroundImageClass}`}
       />
       <Col lg={{ span: 12 }}>
         {/* TODO: language switcher */}
         <p>English</p>
-        <img alt="Pie for Providers logo" src={piefulltanlogo} />
+        <img
+          alt="Pie for Providers logo"
+          src={piefulltanlogo}
+          className="w-24 medium:w-48 mx-auto"
+        />
         {rightColumnContent()}
       </Col>
     </Row>
