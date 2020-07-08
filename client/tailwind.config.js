aec9ec42bec84ed4eb51989b6f1920ad4aeb18a6
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2020-07-07 20:12:50 -0500

    re-add tailwind to support Ant components!

---
+++ client/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: ['./src/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: '#006C9E',
+        blue2: '#004A6E',
+        blue3: '#E2EBF3',
+        blue4: '#F3F8FA',
+        gray1: '#333333',
+        gray2: '#4F4F4F',
+        gray3: '#828282',
+        gray4: '#3B3B3B',
+        gray5: '#E0E0E0',
+        darkGray: '#676767',
+        lightGray: '#979797',
+        white: '#FFFFFF',
+        red1: '#D64B3A',
+        red2: '#F9E4E1',
+        orange1: '#F8921F',
+        orange2: '#FAE7D1',
+        green1: '#179D57',
+        green2: '#DCFAEA',
+        blueOverlay: '#004A6E80'
+      },
+      fontFamily: {
+        proxima: 'Proxima Nova'
+      },
+      screens: {
+        medium: '768px',
+        large: '1200px'
+      }
+    }
+  },
+  plugins: []
+}
