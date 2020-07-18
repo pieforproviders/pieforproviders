488e161582704189e276f00d51d643622f561e94
Jon Trainor <jon.trainor@gmail.com>
2020-07-18 16:23:59 -0500

    add test

---
+++ client/src/Signup/__tests__/Confirmation.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { mount } from 'enzyme'
+import Confirmation from '../Confirmation'
+
+describe('<Confirmation />', () => {
+  let wrapper
+  it('renders the signup confirmation page', () => {
+    wrapper = mount(
+      <MemoryRouter initialEntries={['/']} initialIndex={0}>
+        <Confirmation />
+      </MemoryRouter>
+    )
+    expect(wrapper.text()).toEqual(expect.stringContaining('Thanks for signing up'))
+  })
+})
