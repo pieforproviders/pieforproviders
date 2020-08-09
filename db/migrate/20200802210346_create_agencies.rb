ebea71229ff82e8d223bc26ab690833ff9d91b6c
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2020-08-09 10:56:24 -0700

    db migrate: state cannot be null
    Co-authored-by: Kate Donaldson <kate@katelovescode.com>

--- db/migrate/20200802210346_create_agencies.rb
+++ db/migrate/20200802210346_create_agencies.rb
@@ -2,7 +2,7 @@ class CreateAgencies < ActiveRecord::Migration[6.0]
   def change
     create_table :agencies, id: :uuid do |t|
       t.string :name, null: false
-      t.string :state
+      t.string :state, null: false
       t.boolean :active, null: false, default: true
 
       t.timestamps
