6df962c44db02a478bbd43e43b4fb4a9030c0b5d
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-08-09 11:21:57 -0700

    Agency < UuidApplicationRecord

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # Agencies provide child care subsidy payments
-class Agency < ApplicationRecord
-  # Handles UUIDs breaking ActiveRecord's usual ".first" and ".last" behavior
-  self.implicit_order_column = 'created_at'
-
+#  Note that this class is 'read only' for client apps.
+#  Data required for Agencies will be added to the db via rake tasks or other external means.
+class Agency < UuidApplicationRecord
   validates :active, inclusion: { in: [true, false] }
   validates :name, presence: true
 end
@@ -16,7 +15,7 @@ end
 #  id         :uuid             not null, primary key
 #  active     :boolean          default(TRUE), not null
 #  name       :string           not null
-#  state      :string
+#  state      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
