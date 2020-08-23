5991aeba005ae236ba5ded9b786faea9d438ab8c
Kate Donaldson <kate@katelovescode.com>
2020-08-22 20:47:58 -0500

    Rails root join preferred

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Mailer Overrides for Devise default emails
 class DeviseCustomMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
@@ -7,7 +8,7 @@ class DeviseCustomMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts = {})
     @greeting_name = record.greeting_name || ''
     opts[:subject] = 'Pie for Providers email verification'
-    attachments.inline['pieFullTanLogo.svg'] = File.read("#{Rails.root}/app/views/devise/mailer/assets/pieFullTanLogo.svg")
+    attachments.inline['pieFullTanLogo.svg'] = File.read(Rails.root.join('app', 'views', 'devise', 'mailer', 'assets', 'pieFullTanLogo.svg'))
     super
   end
 end
