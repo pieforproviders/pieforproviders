8eab9f5c61d564b309afe3e8856d9f3b24ccabdf
Kate Donaldson <kate@katelovescode.com>
2020-08-27 08:37:08 -0500

    Update config/initializers/sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 if Rails.env.production?
   Raven.configure do |config|
-    config.dsn = ENV.fetch('SENTRY_DSN_BACKEND', '')
+    config.dsn = ENV.fetch('RAILS_APP_SENTRY_DSN', '')
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
