e3c3e3c63583ecd130a29f9f5693441e9a1dac9c
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-08-30 08:59:56 -0700

    rename to Licenses; use .types as class method

--- app/models/licenses.rb
+++ app/models/licenses.rb
@@ -3,14 +3,14 @@
 # License types
 # These are the list of valid license types.
 # These correspond to an ENUM type defined in the Postgres DB
-class LicenseTypes
+class Licenses
   LICENSE_TYPES = %w[licensed_center
                      licensed_family_home
                      licensed_group_home
                      license_exempt_home
                      license_exempt_center].freeze
 
-  def self.valid_types
+  def self.types
     LICENSE_TYPES.to_h { |s| [s.to_sym, s] }
   end
 end
--- spec/models/licenses_spec.rb
+++ spec/models/licenses_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative File.join(__dir__, '..', '..', 'app', 'models', 'license_types')
+require_relative File.join(__dir__, '..', '..', 'app', 'models', 'licenses')
 
-RSpec.describe LicenseTypes do
-  it '.valid_types' do
+RSpec.describe Licenses do
+  it '.types' do
     expected_values = %w[licensed_center
                          licensed_family_home
                          licensed_group_home
                          license_exempt_home
                          license_exempt_center].freeze
-    expect(described_class.valid_types).to match(expected_values.to_h { |value| [value.to_sym, value] })
+    expect(described_class.types).to match(expected_values.to_h { |value| [value.to_sym, value] })
   end
 end
