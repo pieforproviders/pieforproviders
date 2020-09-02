5f8f87e6de7522ef26f65504977bdf3edb899cbe
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-09-01 17:33:31 -0700

    create SubsidyRule model; updated structure.sql

---
+++ spec/models/subsidy_rule_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SubsidyRule, type: :model do
+  it { should belong_to(:county) }
+  it { should belong_to(:state) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_numericality_of(:max_age).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to monetize(:full_day_rate) }
+  it { should validate_numericality_of(:full_day_rate).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to monetize(:part_day_rate) }
+  it { should validate_numericality_of(:part_day_rate).is_greater_than_or_equal_to(0.00) }
+
+  it { is_expected.to validate_numericality_of(:full_day_max_hours).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to validate_numericality_of(:part_day_max_hours).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to validate_numericality_of(:full_day_threshold).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to validate_numericality_of(:part_day_threshold).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to validate_numericality_of(:full_plus_full_day_max_hours).is_greater_than_or_equal_to(0.00) }
+  it { is_expected.to validate_numericality_of(:full_plus_part_day_max_hours).is_greater_than_or_equal_to(0.00) }
+
+  it {
+    should define_enum_for(:license_type).with_values(
+      described_class::LICENSE_TYPES.to_h { |s| [s, s] }
+    ).backed_by_column_of_type(:string)
+  }
+
+  # enable the example below when PR 252 is merged
+  xit do
+    should define_enum_for(:provider_type).with_values(
+      Licenses.types
+    ).backed_by_column_of_type(:enum)
+  end
+
+  it 'qris rating can be nil' do
+    subsidy_rule = build(:subsidy_rule)
+    subsidy_rule.update(qris_rating: '1')
+    expect(subsidy_rule.valid?).to be_truthy
+    subsidy_rule.qris_rating = nil
+    expect(subsidy_rule.valid?).to be_truthy
+  end
+end
+
+# == Schema Information
+#
+# Table name: subsidy_rules
+#
+#  id                           :uuid             not null, primary key
+#  full_day_max_hours           :decimal(, )      not null
+#  full_day_rate_cents          :integer          default(0), not null
+#  full_day_rate_currency       :string           default("USD"), not null
+#  full_day_threshold           :decimal(, )      not null
+#  full_plus_full_day_max_hours :decimal(, )      not null
+#  full_plus_part_day_max_hours :decimal(, )      not null
+#  max_age                      :decimal(, )      not null
+#  name                         :string           not null
+#  part_day_max_hours           :decimal(, )      not null
+#  part_day_rate_cents          :integer          default(0), not null
+#  part_day_rate_currency       :string           default("USD"), not null
+#  part_day_threshold           :decimal(, )      not null
+#  provider_type                :enum             not null
+#  qris_rating                  :string
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  county_id                    :uuid             not null
+#  state_id                     :uuid             not null
+#
+# Indexes
+#
+#  index_subsidy_rules_on_county_id  (county_id)
+#  index_subsidy_rules_on_state_id   (state_id)
+#
