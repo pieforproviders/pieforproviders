43c732b0ab5d0f2b162300c53a2b4ed71d54b126
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-09-03 11:02:10 -0700

    structure.sql updated after merge

--- db/structure.sql
+++ db/structure.sql
@@ -10,6 +10,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -761,7 +775,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200825180200'),
 ('20200825180300'),
 ('20200828013851'),
-('20200830212138');
+('20200830212138'),
 ('20200902182940');
 
 
