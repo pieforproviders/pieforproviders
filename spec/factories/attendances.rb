3a38467e56b08fda5135edb9d0941ee7783d0d03
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-09-08 12:38:06 -0700

    update attendance annotations

--- spec/factories/attendances.rb
+++ spec/factories/attendances.rb
@@ -7,3 +7,25 @@ FactoryBot.define do
     length_of_care { Attendance::LENGTHS_OF_CARE.sample }
   end
 end
+
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id                  :uuid             not null, primary key
+#  length_of_care      :enum             default("full_day"), not null
+#  slug                :string           not null
+#  starts_on           :date             not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  child_case_cycle_id :uuid             not null
+#
+# Indexes
+#
+#  index_attendances_on_child_case_cycle_id  (child_case_cycle_id)
+#  index_attendances_on_slug                 (slug) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (child_case_cycle_id => child_case_cycles.id)
+#
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -29,3 +29,25 @@ RSpec.describe Attendance, type: :model do
     expect(attend).to be_valid
   end
 end
+
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id                  :uuid             not null, primary key
+#  length_of_care      :enum             default("full_day"), not null
+#  slug                :string           not null
+#  starts_on           :date             not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  child_case_cycle_id :uuid             not null
+#
+# Indexes
+#
+#  index_attendances_on_child_case_cycle_id  (child_case_cycle_id)
+#  index_attendances_on_slug                 (slug) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (child_case_cycle_id => child_case_cycles.id)
+#
