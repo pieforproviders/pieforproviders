487803de5eacbbb7404359307970be34dd3539ec
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-09-08 12:28:04 -0700

    add open Q, answer re: length of care calc'd? adjustable? in comments

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 # Attendance of a child during a specific cycle for a child case
+#
+# from GitHub comments on the PR to implement this model (PR 320)
+# 2020-09-08: open question from Kate D.:
+#   When someone adds an attendance, are we expecting them to choose what
+#   length of care it is, or are we calculating that by the number of hours
+#   as laid out in their subsidy rule?
+#   If we're calculating it, can the user change it, if for some reason our
+#   calculation gets it wrong?
+#
+#   2020-09-08: Answer from Casey S:
+#   Since we haven't designed these screens yet, I don't think we have clear
+#   answers. My assumption, though, is that we're calculating it for them based
+#   on the number of hours, as laid out in their subsidy rule.
+#   I'm not sure if we'll let them change it.
+#
 class Attendance < UuidApplicationRecord
   belongs_to :child_case_cycle
 
@@ -18,3 +33,25 @@ class Attendance < UuidApplicationRecord
     self.slug = generate_slug("#{SecureRandom.hex}#{id}")
   end
 end
+
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id                  :uuid             not null, primary key
+#  length_of_care      :enum             default("full_day"), not null
+#  slug                :string           not null
+#  starts_on           :date             not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  child_case_cycle_id :uuid             not null
+#
+# Indexes
+#
+#  index_attendances_on_child_case_cycle_id  (child_case_cycle_id)
+#  index_attendances_on_slug                 (slug) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (child_case_cycle_id => child_case_cycles.id)
+#
