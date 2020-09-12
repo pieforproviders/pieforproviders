bdd8e5313f9957bf7a49f70478daca59e32f9f4e
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-09-11 19:58:47 -0700

    Site: use Lookup address objects

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -4,10 +4,11 @@ FactoryBot.define do
   factory :site do
     name { Faker::Name.child_care_sites }
     address { Faker::Address.street_address }
-    city { Faker::Address.city }
-    state { Faker::Address.state_abbr }
-    zip { Faker::Address.zip }
-    county { Faker::Address.city_prefix }
+    state { CreateOrSampleLookup.state }
+    county { CreateOrSampleLookup.county(state: state) }
+    city { CreateOrSampleLookup.city(state: county.state, county: county) }
+    zip { CreateOrSampleLookup.zipcode(state: city.state, city: city) }
+
     qris_rating { (1..5).to_a.push(nil).sample }
     business
   end
@@ -20,16 +21,16 @@ end
 #  id          :uuid             not null, primary key
 #  active      :boolean          default(TRUE), not null
 #  address     :string           not null
-#  city        :string           not null
-#  county      :string           not null
 #  name        :string           not null
 #  qris_rating :string
 #  slug        :string           not null
-#  state       :string           not null
-#  zip         :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  business_id :uuid             not null
+#  city_id     :uuid             not null
+#  county_id   :uuid             not null
+#  state_id    :uuid             not null
+#  zip_id      :uuid             not null
 #
 # Indexes
 #
