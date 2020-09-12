e7a1c7c8257c43c5854906acd39a6e714e6a4526
Arun Kumar Mohan <arunmohandm@gmail.com>
2020-09-12 00:21:57 -0500

    Tweak error messages on the login and reset password forms

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -10,6 +10,7 @@ import ErrorAlert from 'ErrorAlert'
 export function Login() {
   const location = useLocation()
   const [apiError, setApiError] = useState(null)
+  const [apiSuccess, setApiSuccess] = useState(null)
   const [showResetPasswordDialog, setShowResetPasswordDialog] = useState(false)
   const { makeRequest } = useApiResponse()
   let history = useHistory()
@@ -17,17 +18,31 @@ export function Login() {
 
   useEffect(() => {
     if (location.state?.error?.status) {
+      setApiSuccess(null)
       setApiError({
         status: location.state?.error?.status,
         message: location.state?.error?.message,
         attribute: location.state?.error?.attribute,
+        context: location.state?.error?.context,
         type: location.state?.error?.type
       })
       window.history.replaceState(null, '')
     }
   }, [location])
 
+  useEffect(() => {
+    if (location.state?.success) {
+      setApiError(null)
+      setApiSuccess({
+        message: location.state.success.message
+      })
+      window.history.replaceState(null, '')
+    }
+  }, [location])
+
   const onFinish = async values => {
+    setApiSuccess(null)
+    setApiError(null)
     const response = await makeRequest({
       type: 'post',
       url: '/login',
@@ -37,7 +52,10 @@ export function Login() {
       const errorMessage = await response.json()
       setApiError({
         status: response.status,
-        message: errorMessage.error
+        message: errorMessage.error,
+        attribute: errorMessage.attribute,
+        type: errorMessage.type,
+        context: { email: values.email }
       })
     } else {
       localStorage.setItem('pie-token', response.headers.get('authorization'))
@@ -58,17 +76,26 @@ export function Login() {
         {t('or')} <span className="uppercase font-bold">{t('login')}</span>
       </p>
 
-      {apiError?.status && (
+      {apiError && (
         <Alert
           className="mb-2"
-          message={apiError.message}
-          type="error"
-          description={
-            apiError?.attribute ? (
-              <ErrorAlert attribute={apiError.attribute} type={apiError.type} />
-            ) : null
+          message={
+            <ErrorAlert
+              attribute={apiError.attribute}
+              type={apiError.type}
+              context={apiError.context}
+            />
           }
+          type="error"
           data-cy="authError"
+        />
+      )}
+
+      {apiSuccess && (
+        <Alert
+          message={apiSuccess.message}
+          type="success"
+          className="mb-2"
         />
       )}
 
