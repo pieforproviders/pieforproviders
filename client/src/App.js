d73705c7b82bb196a903f5928d717a449e17f5ca
Kate Donaldson <kate@katelovescode.com>
2020-09-21 14:25:53 -0500

    Adds translation to confirmation route/page

--- client/src/App.js
+++ client/src/App.js
@@ -58,8 +58,7 @@ const App = () => {
               </Route>
               <Route
                 path="/confirm"
-                // TODO: needs translation
-                title="Confirm your Account"
+                title={t('confirmYourAccount')}
                 component={Confirmation}
               />
               <AuthorizedRoute
