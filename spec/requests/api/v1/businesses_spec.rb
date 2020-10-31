12a82a7ffdc1d77870d80db0b48ddc54c5e2e3d2
Kate Donaldson <kate@katelovescode.com>
2020-10-31 13:19:05 -0500

    Adds logic to prevent admins from deactivating businesses while they still have active children

--- spec/requests/api/v1/businesses_spec.rb
+++ spec/requests/api/v1/businesses_spec.rb
@@ -4,6 +4,7 @@ require 'swagger_helper'
 
 RSpec.describe 'businesses API', type: :request do
   let!(:user) { create(:confirmed_user) }
+  let!(:admin) { create(:admin) }
   let!(:zipcode) { create(:zipcode) }
   let!(:record_params) do
     {
@@ -29,4 +30,36 @@ RSpec.describe 'businesses API', type: :request do
   it_behaves_like 'admins and resource owners can update a record', Business, 'name', 'Hogwarts School', nil
 
   it_behaves_like 'admins and resource owners can delete a record', Business
+
+  describe '#update' do
+    let(:business_with_cases) { create(:business_with_children, user: owner) }
+    let(:id) { business_with_cases.id }
+    path '/api/v1/businesses/{id}' do
+      parameter name: :id, in: :path, type: :string
+
+      put 'cannot update active on a business with active children' do
+        tags 'businesses'
+
+        produces 'application/json'
+        consumes 'application/json'
+
+        parameter name: :business, in: :body, schema: {
+          '$ref' => '#/components/schemas/updateBusiness'
+        }
+        context 'on the right api version' do
+          include_context 'correct api version header'
+          context 'when authenticated' do
+            context 'admin' do
+              before { sign_in admin }
+
+              response '422', 'Business cannot be updated' do
+                let(:business) { { 'business' => { 'active' => 'false' } } }
+                run_test!
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
