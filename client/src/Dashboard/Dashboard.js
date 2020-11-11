a8ecb528d5d927cbd4ec03f6c9aaa74faa80fcd7
cjhaddad <candice.haddad@gmail.com>
2020-11-11 08:37:10 -0700

    add static summary

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -2,80 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useApiResponse } from '_shared/_hooks/useApiResponse'
 import { useSelector } from 'react-redux'
-import { Table, Typography } from 'antd'
+import { Col, Divider, Row, Table, Typography } from 'antd'
 import '_assets/styles/table-overrides.css'
 
-// const STATICDATA = [
-//   {
-//     key: '1',
-//     childName: 'Bessie Cooper',
-//     caseNumber: '282753',
-//     business: 'Lil Baby Ducklings',
-//     attendanceRate: '97%',
-//     minRevenue: '$1266.5',
-//     maxRevenue: '$1888.47'
-//   },
-//   {
-//     key: '2',
-//     childName: 'Jenny Wilson',
-//     caseNumber: '172922',
-//     business: 'Austin Community Child Care',
-//     attendanceRate: '38%',
-//     minRevenue: '$1266.5',
-//     maxRevenue: '$1888.47'
-//   },
-//   {
-//     key: '3',
-//     childName: 'Brooklyn Simmons',
-//     caseNumber: '282753',
-//     business: 'Goslings Grow',
-//     attendanceRate: '96%',
-//     minRevenue: '$2926.11',
-//     maxRevenue: '$1008.86'
-//   },
-//   {
-//     key: '4',
-//     childName: 'Eleanor Pena',
-//     caseNumber: '363171',
-//     business: 'Austin Community Child Care',
-//     attendanceRate: '30%',
-//     minRevenue: '$342.58',
-//     maxRevenue: '$121.62'
-//   },
-//   {
-//     key: '5',
-//     childName: 'Theresa Webb',
-//     caseNumber: '449003',
-//     business: 'Ravenswood Daycare',
-//     attendanceRate: '110%',
-//     minRevenue: '$4000.90',
-//     maxRevenue: '$5000.80'
-//   },
-//   {
-//     key: '6',
-//     childName: 'Kathryn Murphy',
-//     caseNumber: '295340',
-//     business: 'Austin Community Child Care',
-//     attendanceRate: '50%',
-//     minRevenue: '$1240.90',
-//     maxRevenue: '$2000.50'
-//   },
-//   {
-//     key: '7',
-//     childName: 'Leslie Alexander',
-//     caseNumber: '803162',
-//     business: 'Ravenswood Daycare',
-//     attendanceRate: '52%',
-//     minRevenue: '$1109.90',
-//     maxRevenue: '$2188'
-//   }
-// ]
 
 export function Dashboard() {
-  const [dashboardData, setDashboardData] = useState([])
   const token = useSelector(state => state.auth.token)
   const { makeRequest } = useApiResponse()
   const { t } = useTranslation()
+  const staticSummaryStats = [
+    {
+      title: t('guaranteedRevenue'),
+      stat: '$981',
+      definition: t('guaranteedRevenueDef')
+    },
+    {
+      title: t('potentialRevenue'),
+      stat: '$1,200',
+      definition: t('potentialRevenueDef')
+    },
+    {
+      title: t('maxApprovedRevenue'),
+      stat: '$1200',
+      definition: t('maxApprovedRevenueDef')
+    },
+    {
+      title: t('attendanceRate'),
+      stat: '60%',
+      definition: t('attendanceRateDef')
+    }
+  ]
+  const [businessList, setBusinessList] = useState([])
+  const [summaryStats, setSummaryStats] = useState(staticSummaryStats)
   const onHeaderCell = () => {
     return {
       style: {
@@ -204,9 +162,77 @@ export function Dashboard() {
     <div className="dashboard sm:mx-8">
       <div className="m-2">
         <Typography.Title>{t('dashboardTitle')}</Typography.Title>
-        <Typography.Text className="md-3">
+        <Typography.Text className="md-3 text-base">
           {t('revenueProjections')}
         </Typography.Text>
+      </div>
+      <div className="mx-2 my-10">
+        <Row justify="start">
+          {summaryStats.map((stat, i) => (
+            <>
+              <Col span={4}>
+                <Row>
+                  <Typography.Text>{stat.title}</Typography.Text>
+                </Row>
+                <Row>
+                  <Typography.Text className="text-blue2 text-3xl font-semibold mt-2 mb-6">
+                      {stat.stat}
+                  </Typography.Text>
+                </Row>
+                <Row>
+                  <Typography.Paragraph className="text-xs w-2/3">
+                    {stat.definition}
+                  </Typography.Paragraph>
+                </Row>
+              </Col>
+              {i + 1 === summaryStats.length ? null : (
+                <Divider
+                  style={{ borderWidth: 1, borderColor: '#BDBDBD' }}
+                  className="h-32 mx-8 -ml-2"
+                  type="vertical"
+                />
+              )}
+            </>
+          ))}
+          {/* <Col span={3}>
+            <Row>
+              <Typography.Text>{t('potentialRevenue')}</Typography.Text>
+            </Row>
+            <Row>number</Row>
+            <Row>
+              <Typography.Text>{t('potentialRevenueDef')}</Typography.Text>
+            </Row>
+          </Col>
+          <Divider
+            style={{ borderWidth: 1, borderColor: '#BDBDBD' }}
+            className="h-32 mx-8"
+            type="vertical"
+          />
+          <Col span={3}>
+            <Row>
+              <Typography.Text>{t('maxApprovedRevenue')}</Typography.Text>
+            </Row>
+            <Row>number</Row>
+            <Row>{t('maxApprovedRevenueDef')}</Row>
+          </Col>
+          <Divider
+            style={{ borderWidth: 1, borderColor: '#BDBDBD' }}
+            className="h-32 mx-8"
+            type="vertical"
+          />
+          <Col span={3}>
+            <Row>
+              <Typography.Text>{t('attendanceRate')}</Typography.Text>
+            </Row>
+            <Row>number</Row>
+            <Row>{t('attendanceRateDef')}</Row>
+          </Col>
+          <Divider
+            style={{ borderWidth: 1, borderColor: '#BDBDBD' }}
+            className="h-32 mx-8"
+            type="vertical"
+          /> */}
+        </Row>
       </div>
       <Table
         dataSource={dashboardData}
