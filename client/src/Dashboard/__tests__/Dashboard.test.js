f3f493f9018f559d4ca750425ff38c68ea47695a
cjhaddad <candice.haddad@gmail.com>
2020-11-15 13:41:55 -0700

    add tests

--- client/src/Dashboard/__tests__/Dashboard.test.js
+++ client/src/Dashboard/__tests__/Dashboard.test.js
@@ -21,5 +21,17 @@ describe('<Dashboard />', () => {
     expect(container).toHaveTextContent('Attendance rate')
     expect(container).toHaveTextContent('Min. revenue')
     expect(container).toHaveTextContent('Max. revenue')
+
+    // Stat summary content
+    expect(container).toHaveTextContent('Based on days already attended')
+    expect(container).toHaveTextContent(
+      'Based on attendance expected for the rest of the month'
+    )
+    expect(container).toHaveTextContent(
+      'If all families meet 80% attendance rate'
+    )
+    expect(container).toHaveTextContent(
+      'Based on attendance rate of all children listed'
+    )
   })
 })
