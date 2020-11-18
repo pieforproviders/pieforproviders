9712b43f904e97f8921d8c9375ea69b21e155351
Kate Donaldson <kate@katelovescode.com>
2020-11-18 08:28:59 -0600

    Remove migrations

--- db/migrate/20201112193701_make_copay_cents_optional.rb
+++
@@ -1,6 +0,0 @@
-class MakeCopayCentsOptional < ActiveRecord::Migration[6.0]
-  def change
-    remove_monetize :approvals, :copay
-    add_monetize :approvals, :copay, amount: { null: true, default: nil }
-  end
-end
--- db/migrate/20201118020539_kill_pgcrypto_comment_all_envs.rb
+++
@@ -1,9 +0,0 @@
-class KillPgcryptoCommentAllEnvs < ActiveRecord::Migration[6.0]
-  def up
-    ActiveRecord::Base.connection.execute('COMMENT ON EXTENSION pgcrypto IS NULL;')
-  end
-
-  def down
-    ActiveRecord::Base.connection.execute("COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';")
-  end
-end
