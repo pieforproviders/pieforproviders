3a454ae726d3d433e88786134a33e00a04c55b2b
Kate Donaldson <kate@katelovescode.com>
2020-11-22 13:27:41 -0600

    Fixes the param access for confirmation

--- client/src/Confirmation/Confirmation.js
+++ client/src/Confirmation/Confirmation.js
@@ -13,7 +13,8 @@ export function Confirmation({ location }) {
   useEffect(() => {
     let isSubscribed = true
     const confirm = async () => {
-      const confirmationToken = new URLSearchParams(location.search)
+      const params = new URLSearchParams(location.search)
+      const confirmationToken = params.get('confirmation_token')
       const response = await makeRequest({
         type: 'get',
         url: `confirmation${
