ec167b89c4d271531ff6acec64fb1b59ed0bd740
cjhaddad <candice.haddad@gmail.com>
2020-12-07 15:51:30 -0500

    update tests to mock and test api call

--- client/src/Dashboard/__tests__/Dashboard.test.js
+++ client/src/Dashboard/__tests__/Dashboard.test.js
@@ -12,11 +12,15 @@ const doRender = () => {
 }
 
 describe('<Dashboard />', () => {
+
+  beforeEach(() => jest.spyOn(window, 'fetch'))
+
+  afterEach(() => window.fetch.mockRestore())
+
   it('renders the Dashboard page', async () => {
     const { container } = doRender()
 
     await waitFor(() => {
-      console.log(container)
       expect(screen.getAllByRole('columnheader').length).toEqual(7)
       expect(container).toHaveTextContent('Your dashboard')
       expect(container).toHaveTextContent('Child name')
@@ -25,17 +29,8 @@ describe('<Dashboard />', () => {
       expect(container).toHaveTextContent('Guaranteed revenue')
       expect(container).toHaveTextContent('Max. approved revenue')
 
-      // Stat summary content
-      expect(container).toHaveTextContent('Based on days already attended')
-      expect(container).toHaveTextContent(
-        'Based on attendance expected for the rest of the month'
-      )
-      expect(container).toHaveTextContent(
-        'If all families meet 80% attendance rate'
-      )
-      expect(container).toHaveTextContent(
-        'Based on attendance rate of all children listed'
-      )
+      expect(window.fetch).toHaveBeenCalledTimes(1)
+      expect(window.fetch.mock.calls[0][0]).toBe('/api/v1/case_list_for_dashboard')
     })
   })
 })
