8b92b21c3b3ff049c79297f1c18515a70913e856
cjhaddad <candice.haddad@gmail.com>
2020-12-28 16:46:59 -0500

    add sorters for nebraska table columns and refactor

--- client/src/Dashboard/DashboardTable.js
+++ client/src/Dashboard/DashboardTable.js
@@ -9,7 +9,8 @@ import '_assets/styles/tag-overrides.css'
 
 export default function DashboardTable({ tableData, userState }) {
   const { t } = useTranslation()
-  const columnSorter = (a, b, name) => a[name] < b[name] ? -1 : a[name] > b[name] ? 1 : 0
+  const columnSorter = (a, b, name) =>
+    a[name] < b[name] ? -1 : a[name] > b[name] ? 1 : 0
   const onHeaderCell = () => {
     return {
       style: {
@@ -71,87 +72,88 @@ export default function DashboardTable({ tableData, userState }) {
     })
   }
 
-  const neColConfig = [
-    {
-      children: [
-        {
-          name: 'child',
-          render: renderChild,
-          width: 250,
-          sorter: (a, b) => columnSorter(a.child, b.child, 'childName')
-        }
-      ]
-    },
-    {
-      name: 'attendance',
-      children: [
-        {
-          name: 'fullDays',
-          sorter: (a, b) =>
-            a.fullDays.match(/^\d+/)[0] - b.fullDays.match(/^\d+/)[0]
-        },
-        {
-          name: 'hours',
-          sorter: (a, b) => a.hours.match(/^\d+/)[0] - b.hours.match(/^\d+/)[0]
-        },
-        {
-          name: 'absences',
-          sorter: (a, b) =>
-            a.absences.match(/^\d+/)[0] - b.absences.match(/^\d+/)[0]
-        }
-      ]
-    },
-    {
-      name: 'revenue',
-      children: [
-        {
-          name: 'earnedRevenue',
-          sorter: (a, b) => a.earnedRevenue - b.earnedRevenue
-        },
-        {
-          name: 'estimatedRevenue',
-          sorter: (a, b) => a.estimatedRevenue - b.estimatedRevenue
-        },
-        {
-          name: 'transportationRevenue',
-          sorter: (a, b) =>
-            a.transportationRevenue.match(/([0-9]+.[0-9]{2})/)[0] -
-            b.transportationRevenue.match(/([0-9]+.[0-9]{2})/)[0]
-        }
-      ]
-    }
-  ]
-
-  const defaultColConfig = [
-    { name: 'childName', sorter: (a, b) => columnSorter(a, b, 'childName') },
-    { name: 'cNumber', sorter: (a, b) => columnSorter(a, b, 'cNumber') },
-    { name: 'business', sorter: (a, b) => columnSorter(a, b, 'business') },
-    {
-      name: 'attendanceRate',
-      sorter: (a, b) => a.attendanceRate.rate - b.attendanceRate.rate,
-      render: renderAttendanceRate
-    },
-    {
-      name: 'guaranteedRevenue',
-      sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue
-    },
-    {
-      name: 'potentialRevenue',
-      sorter: (a, b) => a.potentialRevenue - b.potentialRevenue
-    },
-    {
-      name: 'maxApprovedRevenue',
-      sorter: (a, b) => a.maxApprovedRevenue - b.maxApprovedRevenue
-    }
-  ]
+  const columnConfig = {
+    ne: [
+      {
+        children: [
+          {
+            name: 'child',
+            render: renderChild,
+            width: 250,
+            sorter: (a, b) => columnSorter(a.child, b.child, 'childName')
+          }
+        ]
+      },
+      {
+        name: 'attendance',
+        children: [
+          {
+            name: 'fullDays',
+            sorter: (a, b) =>
+              a.fullDays.match(/^\d+/)[0] - b.fullDays.match(/^\d+/)[0]
+          },
+          {
+            name: 'hours',
+            sorter: (a, b) => a.hours.match(/^\d+/)[0] - b.hours.match(/^\d+/)[0]
+          },
+          {
+            name: 'absences',
+            sorter: (a, b) =>
+              a.absences.match(/^\d+/)[0] - b.absences.match(/^\d+/)[0]
+          }
+        ]
+      },
+      {
+        name: 'revenue',
+        children: [
+          {
+            name: 'earnedRevenue',
+            sorter: (a, b) => a.earnedRevenue - b.earnedRevenue
+          },
+          {
+            name: 'estimatedRevenue',
+            sorter: (a, b) => a.estimatedRevenue - b.estimatedRevenue
+          },
+          {
+            name: 'transportationRevenue',
+            sorter: (a, b) =>
+              a.transportationRevenue.match(/([0-9]+.[0-9]{2})/)[0] -
+              b.transportationRevenue.match(/([0-9]+.[0-9]{2})/)[0]
+          }
+        ]
+      }
+    ],
+    default: [
+      { name: 'childName', sorter: (a, b) => columnSorter(a, b, 'childName') },
+      { name: 'cNumber', sorter: (a, b) => columnSorter(a, b, 'cNumber') },
+      { name: 'business', sorter: (a, b) => columnSorter(a, b, 'business') },
+      {
+        name: 'attendanceRate',
+        sorter: (a, b) => a.attendanceRate.rate - b.attendanceRate.rate,
+        render: renderAttendanceRate
+      },
+      {
+        name: 'guaranteedRevenue',
+        sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue
+      },
+      {
+        name: 'potentialRevenue',
+        sorter: (a, b) => a.potentialRevenue - b.potentialRevenue
+      },
+      {
+        name: 'maxApprovedRevenue',
+        sorter: (a, b) => a.maxApprovedRevenue - b.maxApprovedRevenue
+      }
+    ]
+  }
 
   return (
     <Table
       dataSource={tableData}
       columns={
         userState === 'NE'
-          ? generateColumns(neColConfig)
-          : generateColumns(defaultColConfig)
+          ? generateColumns(columnConfig['ne'])
+          : generateColumns(columnConfig['default'])
       }
       bordered={true}
       size={'medium'}
