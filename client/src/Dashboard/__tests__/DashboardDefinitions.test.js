4e074e7e79d647d41b51ca99bc55c535f02082e4
cjhaddad <candice.haddad@gmail.com>
2021-02-05 10:51:52 +0000

    add and update tests

---
+++ client/src/Dashboard/__tests__/DashboardDefinitions.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from 'setupTests'
+import DashboardDefinitions from '../DashboardDefinitions'
+
+const doRender = (props = { activeKey: 1, setActiveKey: () => {} }) => {
+  return render(<DashboardDefinitions {...props} />)
+}
+
+describe('<DashboardDefinitions />', () => {
+  it('renders the DashboardDefinition', () => {
+    const { container } = doRender()
+    expect(container).toHaveTextContent('Definitions')
+    expect(container).toHaveTextContent('Attendance')
+    expect(container).toHaveTextContent('Exceeded limit')
+    expect(container).toHaveTextContent('On track')
+    expect(container).toHaveTextContent('At risk')
+    expect(container).toHaveTextContent('Full days')
+    expect(container).toHaveTextContent('Hours')
+    expect(container).toHaveTextContent('Absences')
+    expect(container).toHaveTextContent('Revenue')
+    expect(container).toHaveTextContent('Earned revenue')
+    expect(container).toHaveTextContent('Estimated revenue')
+    expect(container).toHaveTextContent('Transportation revenue')
+    expect(container).toHaveTextContent('Back to top')
+  })
+})
