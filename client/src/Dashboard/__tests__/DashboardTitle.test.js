9ee175a4022e220cd9d5b727c76c0677694db0dc
cjhaddad <candice.haddad@gmail.com>
2021-03-16 13:34:29 +0000

    fix lint

--- client/src/Dashboard/__tests__/DashboardTitle.test.js
+++ client/src/Dashboard/__tests__/DashboardTitle.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render, waitFor } from 'setupTests'
-import { MemoryRouter } from 'react-router-dom'
 import DashboardTitle from '../DashboardTitle'
 
 const doRender = (
   props = {
-    dates: { asOf: 'Mar 16' }, userState: 'IL', getDashboardData: () => {}
+    dates: { asOf: 'Mar 16' },
+    userState: 'IL',
+    getDashboardData: () => {}
   }
 ) => {
   return render(<DashboardTitle {...props} />)
@@ -25,7 +26,11 @@ describe('<DashboardTitle />', () => {
   })
 
   it('renders the Dashboard page when a user is in state', async () => {
-    const { container } = doRender({ dates: { asOf: 'Mar 16' }, userState: 'NE', getDashboardData: () => {}})
+    const { container } = doRender({
+      dates: { asOf: 'Mar 16' },
+      userState: 'NE',
+      getDashboardData: () => {}
+    })
     await waitFor(() => {
       expect(container).toHaveTextContent('Your dashboard')
       expect(container).toHaveTextContent('Mar 16')
