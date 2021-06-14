5c0364bbbe75907257a09ae56b6b884fdba22eb2
cjhaddad <candice.haddad@gmail.com>
2021-06-14 18:32:12 +0100

    stlye adjustments

--- client/src/Attendance/AttendanceDataCell.js
+++ client/src/Attendance/AttendanceDataCell.js
@@ -1,29 +1,33 @@
 import React from 'react'
-import dayjs from 'dayjs'
+import PropTypes from 'prop-types'
+// import dayjs from 'dayjs'
 import { Checkbox, TimePicker } from 'antd'
 
-export default function AttendanceDataCell() {
+export default function AttendanceDataCell({ record }) {
   const handleChange = (...args) => {
     console.log(args)
   }
+  console.log(record)
+  // eslint-disable-next-line no-debugger
+  debugger
   return (
     <div className="flex">
       <div>
         <p>Check In</p>
         <TimePicker
           use12Hours
-          defaultValue={dayjs()}
           format={'HH:mm'}
           onChange={handleChange}
+          suffixIcon={null}
         />
       </div>
       <div>
         <p>Check Out</p>
         <TimePicker
           use12Hours
-          defaultValue={dayjs()}
           format={'HH:mm'}
           onChange={handleChange}
+          suffixIcon={null}
         />
       </div>
       <div>
@@ -32,4 +36,8 @@ export default function AttendanceDataCell() {
       </div>
     </div>
   )
+}
+
+AttendanceDataCell.propTypes = {
+  record: PropTypes.object.isRequired
 }
