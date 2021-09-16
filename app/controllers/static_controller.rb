9db85348d02583a84999ce83c6b0816050119c91
Kate Donaldson <kate@katelovescode.com>
2021-09-16 18:39:51 -0500

    Rails/ApplicationController

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 # renders the single static fallback file we need to serve routes correctly from React Router
+# our application controller is explicitly API-only so we need rubocop to ignore this
+# rubocop:disable Rails/ApplicationController
 class StaticController < ActionController::Base
   def fallback_index_html
     render file: 'public/index.html'
   end
 end
+# rubocop:enable Rails/ApplicationController
