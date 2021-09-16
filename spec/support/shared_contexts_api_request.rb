7db365d42421258766b4ff65335927903f56ab7f
Kate Donaldson <kate@katelovescode.com>
2021-09-15 21:24:45 -0500

    RSpec/VariableName

--- spec/support/shared_contexts_api_request.rb
+++ spec/support/shared_contexts_api_request.rb
@@ -5,12 +5,16 @@ require 'rails_helper'
 VALID_ACCEPT_V1 = 'application/vnd.pieforproviders.v1+json'
 
 RSpec.shared_context 'correct api version header' do
+  # rubocop:disable RSpec/VariableName
   let!(:Accept) { VALID_ACCEPT_V1 }
+  # rubocop:enable RSpec/VariableName
   let!(:headers) { { 'HTTP_ACCEPT' => VALID_ACCEPT_V1 } }
 end
 
 RSpec.shared_context 'incorrect api version header' do
+  # rubocop:disable RSpec/VariableName
   let!(:Accept) { 'application/vnd.pieforproviders.v21+json' }
+  # rubocop:enable RSpec/VariableName
   let(:headers) { { 'HTTP_ACCEPT' => 'application/vnd.pieforproviders.v21+json' } }
 end
 
