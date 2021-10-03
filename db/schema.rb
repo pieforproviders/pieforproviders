6bc9e4872b09dc9644fab1643f469a6c8d0c1c47
Alex Timofeev <atimofeev@reactant.ru>
2021-10-03 16:10:31 +1030

    Add `deleted_at` to Approval

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_050859) do
+ActiveRecord::Schema.define(version: 2021_10_03_054000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_050859) do
     t.date "expires_on"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "deleted_at"
   end
 
   create_table "attendances", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
