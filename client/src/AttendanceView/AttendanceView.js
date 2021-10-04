5687a4f2ecef1486926a6f336a0fa54cf38e4021
Abdulfatai Aka <abdulfataiaka@gmail.com>
2021-10-04 15:27:48 +0100

    fix console error

--- client/src/AttendanceView/AttendanceView.js
+++ client/src/AttendanceView/AttendanceView.js
@@ -187,13 +187,13 @@ export function AttendanceView() {
               {t('inputAttendance')}
             </Button>
           </div>
-          <p>
+          <div>
             <WeekPicker
               hasNext={dateSelected.day(6) < dayjs().day(0)}
               dateSelected={dateSelected}
               handleDateChange={handleDateChange}
             />
-          </p>
+          </div>
           <Table
             dataSource={attendanceData}
             columns={columns}
