70fda882d1be49f493f1492cf24c63b5f218ddf3
Alex Timofeev <atimofeev@reactant.ru>
2021-10-06 00:43:08 +1030

    Don't set `active: false` for soft deleted business

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -46,7 +46,7 @@ module Api
       # DELETE /businesses/:id
       def destroy
         # soft delete
-        @business.update!(active: false, deleted_at: Time.current)
+        @business.update!(deleted_at: Time.current)
       end
 
       private
