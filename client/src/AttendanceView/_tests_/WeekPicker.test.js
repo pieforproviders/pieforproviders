a9e56146b8ebd2c02a5033027bb71404a706e0f3
cjhaddad <candice.haddad@gmail.com>
2021-10-13 16:03:43 +0100

    update weekpicker tests

--- client/src/AttendanceView/_tests_/WeekPicker.test.js
+++ client/src/AttendanceView/_tests_/WeekPicker.test.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { render, waitFor } from 'setupTests'
 import { WeekPicker } from '../WeekPicker'
+import dayjs from 'dayjs'
+import weekday from 'dayjs/plugin/weekday'
+
+dayjs.extend(weekday)
+const day = dayjs()
 
 const doRender = stateOptions => {
-  return render(
-    <WeekPicker
-      dateSelected={{
-        weekday: (num = 0) => ({ format: () => `${num}` })
-      }}
-    />,
-    stateOptions
-  )
+  return render(<WeekPicker dateSelected={day} />, stateOptions)
 }
 
 describe('<WeekPicker />', () => {
   it('renders content', async () => {
     const { container } = doRender()
     await waitFor(() => {
-      expect(container).toHaveTextContent('0 - 6')
+      expect(container).toHaveTextContent(
+        `${day.weekday(0).format('MMM D')} - ${day
+          .weekday(6)
+          .format('MMM D, YYYY')}`
+      )
     })
   })
 })
