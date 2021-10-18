210f869213e46ee990f4a05a3cefb447b084b982
David Candeau <dcandeau@gmail.com>
2021-10-18 16:34:27 -0400

    added payment input component

---
+++ client/src/Payment/paymentDataCell.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Checkbox, InputNumber } from 'antd'
+import { useTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
+
+export default function PaymentDataCell({ updateTotalPayment, columnIndex }) {
+  const { t } = useTranslation()
+  const [isDifferentPayment, setIsDifferentPayment] = useState(false)
+
+  const currencyInput = (
+    <InputNumber
+      placeholder={t('enterAmount')}
+      formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+      parser={value => value.replace(/\$\s?|(,*)/g, '')}
+      disabled={!isDifferentPayment}
+      onChange={test}
+    />
+  )
+
+  function test(value) {
+    updateTotalPayment(value)
+  }
+
+  const handleIsDifferentPaymentIsSet = e => {
+    setIsDifferentPayment(e.target.checked)
+  }
+
+  return (
+    <div>
+      <Checkbox onChange={handleIsDifferentPaymentIsSet} />
+      {t('differentAmountFromState')} {currencyInput}
+    </div>
+  )
+}
+
+PaymentDataCell.propTypes = {
+  updateTotalPayment: PropTypes.func.isRequired,
+  columnIndex: PropTypes.number.isRequired
+}
--- client/src/Payment/paymentModal.js
+++ client/src/Payment/paymentModal.js
@@ -1,40 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import { Checkbox, InputNumber } from 'antd'
+import { Table } from 'antd'
+import PaymentDataCell from './paymentDataCell'
 
 export function PaymentModal() {
   const { cases } = useSelector(state => state)
   const { t } = useTranslation()
+  const [childPayments] = useState(false)
 
-  const currencyInput = (
-    <InputNumber
-      defaultValue={1000}
-      formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-      parser={value => value.replace(/\$\s?|(,*)/g, '')}
-    />
-  )
+  function initChildPayments() {
+    cases.forEach(child => {
+      childPayments.push(child.guaranteedRevenue)
+    })
+  }
 
-  const table = (
-    <table>
-      <tr>
-        <th>{t('childName')}</th>
-        <th>{t('earnedRevenue')}</th>
-        <th>
-          {t('updatePayment')} ({t('differentPaymentAmount')})
-        </th>
-      </tr>
-      {cases.map(childCase => (
-        <tr key={childCase.id}>
-          <td>{childCase.childName}</td>
-          <td>${childCase.guaranteedRevenue}</td>
-          <td>
-            <Checkbox /> {t('differentAmountFromState')} {currencyInput}
-          </td>
-        </tr>
-      ))}
-    </table>
-  )
+  initChildPayments()
+
+  // const table = (
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>{t('childName')}</th>
+  //         <th>{t('earnedRevenue')}</th>
+  //         <th>
+  //           {t('updatePayment')} ({t('differentPaymentAmount')})
+  //         </th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       {cases.map(childCase => (
+  //         <tr key={childCase.id}>
+  //           <td>{childCase.childName}</td>
+  //           <td>${childCase.guaranteedRevenue}</td>
+  //           <td>
+  //             <Checkbox /> {t('differentAmountFromState')} {currencyInput()}
+  //           </td>
+  //         </tr>
+  //       ))}
+  //     </tbody>
+  //   </table>
+  // )
+
+  const updateTotalPayment = (value, index) => {
+    childPayments[index] = value
+    console.log(childPayments)
+  }
+
+  const columns = [
+    {
+      title: t('childName'),
+      dataIndex: 'childName'
+    },
+    {
+      title: t('earnedRevenue'),
+      dataIndex: 'guaranteedRevenue'
+    },
+    {
+      title: updatePaymentHeader,
+      render: index => {
+        return (
+          <PaymentDataCell
+            updateTotalPayment={updateTotalPayment}
+            columnIndex={index}
+          />
+        )
+      }
+    }
+  ]
+
+  function updatePaymentHeader() {
+    return (
+      <div>
+        {t('updatePayment')} ({t('differentPaymentAmount')})
+      </div>
+    )
+  }
+
+  const table = <Table bordered={false} columns={columns} dataSource={cases} />
 
   return (
     <div>
