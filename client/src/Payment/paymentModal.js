f2cd5fa8a3fef24c263fe6c4b934df2edb22b07a
Huy Q Nguyen <hnguyen4@atlassian.com>
2021-10-22 19:41:07 -0700

    Adding payment month dropdown

--- client/src/Payment/paymentModal.js
+++ client/src/Payment/paymentModal.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import { Table } from 'antd'
+import { Table, Dropdown, Menu } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 import PaymentDataCell from './paymentDataCell'
 import PropTypes from 'prop-types'
 import '_assets/styles/payment-table-overrides.css'
@@ -12,6 +13,7 @@ export function PaymentModal({ setTotalPayment }) {
   const { t } = useTranslation()
   const [currentChildID, setCurrentChildID] = useState(false)
   const [childPayments, setChildPayments] = useState({})
+  const [visible, setVisible] = useState(false)
 
   useEffect(() => {
     initChildPayments()
@@ -110,9 +112,61 @@ export function PaymentModal({ setTotalPayment }) {
     />
   )
 
+  function handleMenuClick(e) {
+    setVisible(false)
+  }
+
+  function handleVisibleChange(flag) {
+    setVisible(flag)
+  }
+  const monthNames = [
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec'
+  ]
+  const current = new Date()
+  current.setMonth(current.getMonth() - 1)
+  const previousMonth = monthNames[current.getMonth()]
+  const previousMonthYear = current.getFullYear()
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="1">
+        {t(previousMonth)} {previousMonthYear}
+      </Menu.Item>
+    </Menu>
+  )
+
   return (
     <div>
       <p>{t('recordAChildsPayment')}</p>
+      <h3>{t('step1')}</h3>
+      <p>{t('choosePaymentMonth')}</p>
+      <Dropdown
+        overlay={menu}
+        onVisibleChange={handleVisibleChange}
+        visible={visible}
+      >
+        <a
+          href={() => false}
+          className="ant-dropdown-link"
+          onClick={e => e.preventDefault()}
+        >
+          {t(previousMonth)} {previousMonthYear}
+          <DownOutlined />
+        </a>
+      </Dropdown>
+      <h3>{t('step2')}</h3>
+      <p>{t('childrenPayment')}</p>
       {table}
     </div>
   )
