b8a4083b0e59ca0c7a07d41c4fc01f39c6fdc477
Kate Donaldson <kate@katelovescode.com>
2021-12-16 14:28:03 -0600

    Selected underline

--- client/src/_shared/__tests__/Header.test.js
+++ client/src/_shared/__tests__/Header.test.js
@@ -18,5 +18,18 @@ describe('<Header />', () => {
 
     fireEvent.click(element)
     expect(element).toHaveTextContent(/Español/)
+
+    let attendance_button = screen.getByText(/Attendance/)
+    let dashboard_button = screen.getByText(/Dashboard/)
+    fireEvent.click(attendance_button)
+    expect(attendance_button).closest('div').toHaveClass('border-primaryBlue')
+    expect(dashboard_button)
+      .closest('div')
+      .not.toHaveClass('border-primaryBlue')
+    fireEvent.click(dashboard_button)
+    expect(attendance_button)
+      .closest('div')
+      .not.toHaveClass('border-primaryBlue')
+    expect(dashboard_button).closest('div').toHaveClass('border-primaryBlue')
   })
 })
