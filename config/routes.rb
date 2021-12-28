494fad3b345663920f7ab39d37b9c0ea3f021f63
Kate Donaldson <kate@katelovescode.com>
2021-12-28 13:05:02 -0600

    Removes accidental commits from other envs

--- config/routes.rb
+++ config/routes.rb
@@ -77,6 +77,8 @@ end
 #                                          PUT    /api/v1/children/:id(.:format)                                                                    api/v1/children#update {:format=>:json}
 #                                          DELETE /api/v1/children/:id(.:format)                                                                    api/v1/children#destroy {:format=>:json}
 #                              attendances GET    /api/v1/attendances(.:format)                                                                     api/v1/attendances#index {:format=>:json}
+#                               attendance PATCH  /api/v1/attendances/:id(.:format)                                                                 api/v1/attendances#update {:format=>:json}
+#                                          PUT    /api/v1/attendances/:id(.:format)                                                                 api/v1/attendances#update {:format=>:json}
 #                             service_days GET    /api/v1/service_days(.:format)                                                                    api/v1/service_days#index {:format=>:json}
 #                       attendance_batches POST   /api/v1/attendance_batches(.:format)                                                              api/v1/attendance_batches#create {:format=>:json}
 #                  case_list_for_dashboard GET    /api/v1/case_list_for_dashboard(.:format)                                                         api/v1/users#case_list_for_dashboard {:format=>:json}
--- db/schema.rb
+++ db/schema.rb
@@ -201,15 +201,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_211754) do
     t.index ["expires_on"], name: "index_nebraska_rates_on_expires_on"
   end
 
-  create_table "payments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.date "month", null: false
-    t.decimal "amount", null: false
-    t.uuid "child_approval_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["child_approval_id"], name: "index_payments_on_child_approval_id"
-  end
-
   create_table "schedules", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
     t.date "effective_on", null: false
     t.date "expires_on"
@@ -297,7 +288,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_211754) do
   add_foreign_key "children", "businesses"
   add_foreign_key "illinois_approval_amounts", "child_approvals"
   add_foreign_key "nebraska_approval_amounts", "child_approvals"
-  add_foreign_key "payments", "child_approvals"
   add_foreign_key "schedules", "children"
   add_foreign_key "service_days", "children"
   add_foreign_key "temporary_nebraska_dashboard_cases", "children"
