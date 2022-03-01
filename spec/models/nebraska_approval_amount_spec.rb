c94d8dd3fedda7ecd171feaf598a6790d04ec284
spencer page <pagespence@yahoo.com>
2022-02-28 22:05:17 -0800

    add expires_on_after_effective_on validation

--- spec/models/nebraska_approval_amount_spec.rb
+++ spec/models/nebraska_approval_amount_spec.rb
@@ -3,10 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe NebraskaApprovalAmount, type: :model do
+  let(:nebraska_approval_amount) { build(:nebraska_approval_amount) }
+
   it { is_expected.to belong_to(:child_approval) }
 
   it 'factory should be valid (default; no args)' do
-    expect(build(:nebraska_approval_amount)).to be_valid
+    expect(nebraska_approval_amount).to be_valid
+  end
+
+  it { is_expected.to validate_presence_of(:expires_on) }
+  it { is_expected.to validate_presence_of(:effective_on) }
+
+  it 'validates expires_on_after_effective_on' do
+    nebraska_approval_amount.update(expires_on: 50.years.ago)
+    expect(nebraska_approval_amount.errors.messages[:expires_on]).to include('must be after the effective on date')
   end
 end
 
