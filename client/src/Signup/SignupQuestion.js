b3ab2e1aa47ab853aa1c19371bf2eaba0e118d2a
cjhaddad <candice.haddad@gmail.com>
2022-03-22 21:37:36 +0000

    add extra vergabe at bottom, styling tweaks

--- client/src/Signup/SignupQuestion.js
+++ client/src/Signup/SignupQuestion.js
@@ -5,7 +5,7 @@ import '_assets/styles/radiobutton-overrides.css'
 
 function SignupQuestion({ onChange, questionText }) {
   return (
-    <div className="signup-question mt-1 mb-6 text-left">
+    <div className="signup-question mt-1 text-left">
       <p className="mb-2 text-gray3 font-normal">{questionText}</p>
       <Radio.Group onChange={onChange}>
         <Radio.Button value="True">True</Radio.Button>
