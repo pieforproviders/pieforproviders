be5f01bd65372ad170d49e55b886b57458111613
nicorinn <16536987+nicorinn@users.noreply.github.com>
2022-03-21 23:33:06 -0500

    Fixed bug with profile page reload

--- client/src/Profile/Profile.js
+++ client/src/Profile/Profile.js
@@ -27,151 +27,169 @@ export function Profile() {
   }
 
   const mobileHeader = (
-    <div className="mb-10">
-      <div className="flex justify-center">
-        <Title>{t('myProfile')}</Title>
-      </div>
-      <div className="flex justify-center">
-        <Avatar className="bg-primaryBlue" size={64} style={{ fontSize: 28 }}>
-          {user.greeting_name[0]}
-        </Avatar>
-      </div>
-    </div>
+    <>
+      {Object.keys(user).length !== 0 && (
+        <div className="mb-10">
+          <div className="flex justify-center">
+            <Title>{t('myProfile')}</Title>
+          </div>
+          <div className="flex justify-center">
+            <Avatar
+              className="bg-primaryBlue"
+              size={64}
+              style={{ fontSize: 28 }}
+            >
+              {user.greeting_name[0]}
+            </Avatar>
+          </div>
+        </div>
+      )}
+    </>
   )
 
   const desktopHeader = (
-    <div className="mb-10">
-      <div className={`flex ${screens.md ? '' : 'justify-between'}`}>
-        <div>
-          <Title style={{ marginBottom: 0 }}>{t('myProfile')}</Title>
+    <>
+      {Object.keys(user).length !== 0 && (
+        <div className="mb-10">
+          <div className={`flex ${screens.md ? '' : 'justify-between'}`}>
+            <div>
+              <Title style={{ marginBottom: 0 }}>{t('myProfile')}</Title>
+            </div>
+            <div className="flex items-center">
+              <IconButton
+                className="ml-5 text-base"
+                icon={<EditIcon />}
+                text={screens.md ? 'Edit' : 'Edit Profile'}
+                onClick={() => null}
+              />
+            </div>
+          </div>
+          <Divider style={{ marginTop: 0 }} />
+          <Space size="middle">
+            <Avatar
+              className="bg-primaryBlue"
+              size={54}
+              style={{ fontSize: 28 }}
+            >
+              {user.greeting_name[0]}
+            </Avatar>
+            <div>
+              <Title level={2} style={{ margin: 0 }}>
+                {user.full_name}
+              </Title>
+              {business && (
+                <span className="text-lg font-semibold text-black">
+                  {business.name}
+                </span>
+              )}
+            </div>
+          </Space>
         </div>
-        <div className="flex items-center">
-          <IconButton
-            className="ml-5 text-base"
-            icon={<EditIcon />}
-            text={screens.md ? 'Edit' : 'Edit Profile'}
-            onClick={() => null}
-          />
-        </div>
-      </div>
-      <Divider style={{ marginTop: 0 }} />
-      <Space size="middle">
-        <Avatar className="bg-primaryBlue" size={54} style={{ fontSize: 28 }}>
-          {user.greeting_name[0]}
-        </Avatar>
-        <div>
-          <Title level={2} style={{ margin: 0 }}>
-            {user.full_name}
-          </Title>
-          {business && (
-            <span className="text-lg font-semibold text-black">
-              {business.name}
-            </span>
-          )}
-        </div>
-      </Space>
-    </div>
+      )}
+    </>
   )
 
   return (
-    <div className={screens.sm ? 'pt-0 p-5' : 'pt-0 p-2'}>
-      {screens.xs && mobileHeader}
-      {screens.sm && desktopHeader}
-      <Row gutter={[16, 32]}>
-        <Col span={getColSpan()}>
-          <Space direction="vertical" width="100%" size="middle">
-            <Title level={5} style={{ marginBottom: 0 }}>
-              {t('personalDetails')}
-            </Title>
-            {screens.xs && (
-              <Space direction="vertical">
-                <Text strong>{t('fullName')}</Text>
-                <Text>{user.full_name}</Text>
-              </Space>
-            )}
-            <Space direction="vertical">
-              <Text strong>{t('preferredName')}</Text>
-              <Text>{user.greeting_name}</Text>
-            </Space>
-            <Space direction="vertical">
-              <Text strong>{t('phone')}</Text>
-              <Text>{user.phone_number}</Text>
-            </Space>
-            <Space direction="vertical">
-              <Text strong>{t('emailAddress')}</Text>
-              <Text>{user.email}</Text>
-            </Space>
-            <Space direction="vertical">
-              <Text strong>{t('preferredLanguage')}</Text>
-              <Text>
-                {user.language === 'es' ? t('spanish') : t('english')}
-              </Text>
-            </Space>
-            {!screens.xs && (
-              <IconButton
-                icon={<LockIcon />}
-                text="Change Password"
-                onClick={() => null}
-              />
-            )}
-          </Space>
-        </Col>
-        {business && (
-          <Col span={getColSpan()} style={{}}>
-            <Space direction="vertical" size="middle">
+    Object.keys(user).length !== 0 && (
+      <div className={screens.sm ? 'pt-0 p-5' : 'pt-0 p-2'}>
+        {screens.xs && mobileHeader}
+        {screens.sm && desktopHeader}
+        <Row gutter={[16, 32]}>
+          <Col span={getColSpan()}>
+            <Space direction="vertical" width="100%" size="middle">
               <Title level={5} style={{ marginBottom: 0 }}>
-                {t('businessDetails')}
+                {t('personalDetails')}
               </Title>
               {screens.xs && (
                 <Space direction="vertical">
-                  <Text strong>{t('organizationName')}</Text>
-                  <Text>{business.name}</Text>
+                  <Text strong>{t('fullName')}</Text>
+                  <Text>{user.full_name}</Text>
                 </Space>
               )}
               <Space direction="vertical">
-                <Text strong>{t('accountType')}</Text>
+                <Text strong>{t('preferredName')}</Text>
+                <Text>{user.greeting_name}</Text>
+              </Space>
+              <Space direction="vertical">
+                <Text strong>{t('phone')}</Text>
+                <Text>{user.phone_number}</Text>
+              </Space>
+              <Space direction="vertical">
+                <Text strong>{t('emailAddress')}</Text>
+                <Text>{user.email}</Text>
+              </Space>
+              <Space direction="vertical">
+                <Text strong>{t('preferredLanguage')}</Text>
                 <Text>
-                  {isMultiBusiness
-                    ? t('multiBusinessProfile')
-                    : t('singleBusinessProfile')}
+                  {user.language === 'es' ? t('spanish') : t('english')}
                 </Text>
               </Space>
-              <Space direction="vertical">
-                <Text strong>{t('licenseType')}</Text>
-                <Text>{business.license_type}</Text>
-              </Space>
-              <Space direction="vertical">
-                <Text strong>{t('location')}</Text>
-                <Text>{`${business.county}, ${business.zipcode}`}</Text>
-              </Space>
-              <Space direction="vertical">
-                <Text strong>{t('qrisRating')}</Text>
-                <Text>{business.qris_rating}</Text>
-              </Space>
-              <Space direction="vertical">
-                <Text strong>{t('accreditedQuestion')}</Text>
-                <Text>{business.accredited ? 'Yes' : 'No'}</Text>
-              </Space>
+              {!screens.xs && (
+                <IconButton
+                  icon={<LockIcon />}
+                  text="Change Password"
+                  onClick={() => null}
+                />
+              )}
             </Space>
           </Col>
-        )}
-        {screens.xs && (
-          <Col>
-            <Space direction="vertical" align="start">
-              <IconButton
-                icon={<EditIcon />}
-                text="Edit Profile"
-                onClick={() => null}
-              />
-              <IconButton
-                icon={<LockIcon />}
-                text="Change Password"
-                onClick={() => null}
-              />
-            </Space>
-          </Col>
-        )}
-      </Row>
-    </div>
+          {business && (
+            <Col span={getColSpan()} style={{}}>
+              <Space direction="vertical" size="middle">
+                <Title level={5} style={{ marginBottom: 0 }}>
+                  {t('businessDetails')}
+                </Title>
+                {screens.xs && (
+                  <Space direction="vertical">
+                    <Text strong>{t('organizationName')}</Text>
+                    <Text>{business.name}</Text>
+                  </Space>
+                )}
+                <Space direction="vertical">
+                  <Text strong>{t('accountType')}</Text>
+                  <Text>
+                    {isMultiBusiness
+                      ? t('multiBusinessProfile')
+                      : t('singleBusinessProfile')}
+                  </Text>
+                </Space>
+                <Space direction="vertical">
+                  <Text strong>{t('licenseType')}</Text>
+                  <Text>{business.license_type}</Text>
+                </Space>
+                <Space direction="vertical">
+                  <Text strong>{t('location')}</Text>
+                  <Text>{`${business.county}, ${business.zipcode}`}</Text>
+                </Space>
+                <Space direction="vertical">
+                  <Text strong>{t('qrisRating')}</Text>
+                  <Text>{business.qris_rating}</Text>
+                </Space>
+                <Space direction="vertical">
+                  <Text strong>{t('accreditedQuestion')}</Text>
+                  <Text>{business.accredited ? 'Yes' : 'No'}</Text>
+                </Space>
+              </Space>
+            </Col>
+          )}
+          {screens.xs && (
+            <Col>
+              <Space direction="vertical" align="start">
+                <IconButton
+                  icon={<EditIcon />}
+                  text="Edit Profile"
+                  onClick={() => null}
+                />
+                <IconButton
+                  icon={<LockIcon />}
+                  text="Change Password"
+                  onClick={() => null}
+                />
+              </Space>
+            </Col>
+          )}
+        </Row>
+      </div>
+    )
   )
 }
