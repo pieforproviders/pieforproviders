3812b32ef451d622e67e84e83e8dc901a4d69227
Kate Donaldson <kate@katelovescode.com>
2022-03-23 10:54:53 -0500

    Updates Cypress test with new fields

--- client/src/Signup/SignupQuestion.js
+++ client/src/Signup/SignupQuestion.js
@@ -4,22 +4,31 @@ import { Radio } from 'antd'
 import { useTranslation } from 'react-i18next'
 import '_assets/styles/radiobutton-overrides.css'
 
-function SignupQuestion({ onChange, questionText }) {
+function SignupQuestion({ onChange, questionText, tag }) {
   const { t } = useTranslation()
   return (
-    <div className="signup-question mt-1 text-left">
-      <p className="mb-2 text-gray3 font-normal">{questionText}</p>
+    <div className="mt-1 text-left signup-question">
+      <p className="mb-2 font-normal text-gray3">{questionText}</p>
       <Radio.Group onChange={onChange}>
-        <Radio.Button value="True">{t('true')}</Radio.Button>
-        <Radio.Button value="Mostly True">{t('mostyleTrue')}</Radio.Button>
-        <Radio.Button value="Mostly False">{t('mostlyFalse')}</Radio.Button>
-        <Radio.Button value="False">{t('false')}</Radio.Button>
+        <Radio.Button value="True" data-cy={`${tag}-true`}>
+          {t('true')}
+        </Radio.Button>
+        <Radio.Button value="Mostly True" data-cy={`${tag}-mostly-true`}>
+          {t('mostlyTrue')}
+        </Radio.Button>
+        <Radio.Button value="Mostly False" data-cy={`${tag}-mostly-false`}>
+          {t('mostlyFalse')}
+        </Radio.Button>
+        <Radio.Button value="False" data-cy={`${tag}-false`}>
+          {t('false')}
+        </Radio.Button>
       </Radio.Group>
     </div>
   )
 }
 
 SignupQuestion.propTypes = {
+  tag: PropTypes.string,
   onChange: PropTypes.func,
   questionText: PropTypes.string
 }
