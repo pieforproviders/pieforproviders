f7a821a9eb5920f892cf3824e88c252a1bba27cc
Kate Donaldson <kate@katelovescode.com>
2022-03-23 17:45:54 -0500

    More fixes to the migration

--- db/migrate/20220317035601_add_first_and_last_name_to_child.rb
+++ db/migrate/20220317035601_add_first_and_last_name_to_child.rb
@@ -5,7 +5,13 @@ class AddFirstAndLastNameToChild < ActiveRecord::Migration[6.1]
 
     Child.all.each do |c|
       name = c.full_name.split(' ')
-      c.update!(first_name: name.first, last_name: name.last)
+      full_name = c.full_name.gsub("'", "''")
+      first_name = name.first.gsub("'", "''").presence || '-'
+      last_name = name.last.gsub("'", "''").presence || '-'
+      ActiveRecord::Base.connection.execute("update children set first_name = '#{first_name}', last_name = '#{last_name}' where full_name = '#{full_name}'")
+      # TODO: for some reason, updating the child record is executing callbacks on attendances and service days and deleting a lot of records on production data
+      # I'm not sure what's triggering it after about 30 mins of investigation so I'm doing SQL directly
+      # c.update!(first_name: name.first, last_name: name.last)
     end
   end
 
