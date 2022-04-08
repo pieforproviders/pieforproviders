466a1c698c196c64eee01af80e6bfd866f1df50d
Kate Donaldson <kate@katelovescode.com>
2022-04-08 17:37:44 -0500

    Remove old debugger disable comments

--- client/src/ComingSoon/ComingSoon.js
+++ client/src/ComingSoon/ComingSoon.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-debugger */
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import pieWithSlice from '../_assets/pieWithSlice.png'
@@ -7,12 +6,12 @@ export function ComingSoon() {
   const { t } = useTranslation()
 
   return (
-    <div className="text-center h-full">
+    <div className="h-full text-center">
       <div>
         <img className="m-auto" alt="pieWithSlice" src={pieWithSlice} />
       </div>
-      <h2 className="h2-small mt-6">{t('comingSoonMsg1')}</h2>
-      <h2 className="h2-small xs:mt-3 mt-1 mb-5">{t('comingSoonMsg2')}</h2>
+      <h2 className="mt-6 h2-small">{t('comingSoonMsg1')}</h2>
+      <h2 className="mt-1 mb-5 h2-small xs:mt-3">{t('comingSoonMsg2')}</h2>
     </div>
   )
 }
