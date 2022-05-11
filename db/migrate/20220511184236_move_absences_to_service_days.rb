fc62c64ec4d81ad8c965b1eb178a0448ed57e5c1
Kate Donaldson <kate@katelovescode.com>
2022-05-11 17:19:28 -0500

    Corrects the migration for service_day absences

--- db/migrate/20220511184236_move_absences_to_service_days.rb
+++ db/migrate/20220511184236_move_absences_to_service_days.rb
@@ -1,7 +1,10 @@
 class MoveAbsencesToServiceDays < ActiveRecord::Migration[6.1]
   def change
-    Attendance.all.where.not(absence: nil) do |attendance|
-      attendance.service_day.update(absence_type: attendance.absence)
+    Attendance.all.where.not(absence: nil).in_batches do |batch|
+      batch.each do |attendance|
+        attendance.service_day.schedule && attendance.service_day.update!(absence_type: attendance.absence)
+        attendance.destroy!
+      end
     end
   end
 end
