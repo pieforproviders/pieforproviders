582ca91d389f42d708aebb2d5b634b758c779ec0
Kate Donaldson <kate@katelovescode.com>
2022-05-13 13:09:23 -0500

    Background job

--- db/migrate/20220511184236_move_absences_to_service_days.rb
+++ db/migrate/20220511184236_move_absences_to_service_days.rb
@@ -2,9 +2,17 @@ class MoveAbsencesToServiceDays < ActiveRecord::Migration[6.1]
   def change
     Attendance.all.where.not(absence: nil).in_batches do |batch|
       batch.each do |attendance|
-        attendance.service_day.schedule && attendance.service_day.update!(absence_type: attendance.absence)
-        attendance.destroy!
+        AttendanceAbsenceCorrectionJob.perform_later(attendance)
       end
+    end
+  end
+
+  class AttendanceAbsenceCorrectionJob < ApplicationJob
+    queue_as :default
+
+    def perform(attendance)
+      attendance.service_day.schedule && attendance.service_day.update!(absence_type: attendance.absence)
+      attendance.destroy!
     end
   end
 end
