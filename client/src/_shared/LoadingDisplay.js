bcb02f1fc0ae7ad1f52baa7d41781b47242e996b
cjhaddad <candice.haddad@gmail.com>
2022-05-26 10:20:10 -0400

    add transparent and center pie slice image and animation styles

--- client/src/_shared/LoadingDisplay.js
+++ client/src/_shared/LoadingDisplay.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import pieWithSlice from '../_assets/pieWithSlice.png'
+import slicedPie from '../_assets/slicedPieTransparent.png'
+import '_assets/styles/loading-spin.css'
 import '_assets/styles/progress-bar-overrides.css'
 
 export function LoadingDisplay() {
   return (
     <div>
-      <img
-        className="m-auto animate-spin"
-        alt="pieWithSlice"
-        src={pieWithSlice}
-      />
+      <img className="m-auto loading-spin" alt="pieWithSlice" src={slicedPie} />
       <div className="my-7">
         <h3 className="h2-large">Loading...</h3>
       </div>
