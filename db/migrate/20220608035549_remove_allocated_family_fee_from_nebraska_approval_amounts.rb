42a521e30e1603c4ac68eae75c125bdcaead597f
spencer page <pagespence@yahoo.com>
2022-06-09 04:05:28 -0700

    refactor migration to change method

--- db/migrate/20220608035549_remove_allocated_family_fee_from_nebraska_approval_amounts.rb
+++ db/migrate/20220608035549_remove_allocated_family_fee_from_nebraska_approval_amounts.rb
@@ -1,9 +1,5 @@
 class RemoveAllocatedFamilyFeeFromNebraskaApprovalAmounts < ActiveRecord::Migration[6.1]
-  def up
-    remove_column :nebraska_approval_amounts, :allocated_family_fee
-  end
-
-  def down
-    add_column :nebraska_approval_amounts, :allocated_family_fee, :decimal
+  def change
+    remove_column :nebraska_approval_amounts, :allocated_family_fee, :decimal
   end
 end
