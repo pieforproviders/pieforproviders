bc181663849af9c8f923165dee743bb764c9e96d
cjhaddad <candice.haddad@gmail.com>
2022-06-14 20:31:13 +0100

    tweak notifications to use List component and not be collapsable, use notifications endpoint format, tweak styling for dashboard stats

--- client/src/Dashboard/Notifications.js
+++ client/src/Dashboard/Notifications.js
@@ -1,40 +1,86 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Collapse, Divider } from 'antd'
-import { ExclamationCircleOutlined } from '@ant-design/icons'
-
-const { Panel } = Collapse
+import { Divider, List } from 'antd'
+import dayjs from 'dayjs'
+import { ExclamationCircleOutlined, MailOutlined } from '@ant-design/icons'
+import { PIE_FOR_PROVIDERS_EMAIL } from '../constants'
 
 const mockMessages = [
   {
-    message:
-      "Eleanor Pena's subsidy approval is expiring on May 31st Email the updated letter to team@pieforproviders.com",
-    date: 'May 1, 2022'
+    first_name: 'Jasveen',
+    last_name: 'Khirwar',
+    effective_on: 'Mon, 06 Jun 2022 18:01:18.814736000 UTC +00:00',
+    expiration_date: 'Mon, 01 Jun 2022 18:01:18.814736000 UTC +00:00'
   },
   {
-    message:
-      "Jenny Wilson's subsidy approval is expiring on May 31st Email the updated letter to team@pieforproviders.com",
-    date: 'May 1, 2022'
+    first_name: 'Jane',
+    last_name: 'Queen',
+    effective_on: 'Mon, 06 Jun 2022 18:01:18.814736000 UTC +00:00',
+    expiration_date: 'Mon, 01 Jun 2022 18:01:18.814736000 UTC +00:00'
   }
 ]
 
 const Notifications = ({ messages = mockMessages }) => (
-  <Collapse>
-    <Panel header={`NOTIFICATIONS (${messages.length})`}>
-      <div>
-        {messages.map(({ message, date }, index) => (
-          <div key={index} className="flex items-start">
-            <ExclamationCircleOutlined className="mr-3 text-red-500 text-xl" />
-            <div className="inline-block mt-1">
-              <div>{message}</div>
-              <div className="mt-1 text-gray-400">{date}</div>
-              <Divider />
-            </div>
-          </div>
-        ))}
+  <List
+    className="bg-blue4 px-8 md:ml-4 md:w-2/3 xl:w-3/4 mt-4"
+    header={
+      <div className="font-semibold text-lg">
+        <p>{`NOTIFICATIONS ${
+          messages.length > 0 ? `(${messages.length})` : ''
+        }`}</p>
+        <Divider />
       </div>
-    </Panel>
-  </Collapse>
+    }
+    dataSource={messages}
+    locale={{
+      emptyText: (
+        <div className="flex">
+          <MailOutlined
+            style={{
+              color: '#676767',
+              fontSize: '2.25rem'
+            }}
+          />
+          <div className="ml-3 text-gray4">
+            <p className="flex font-bold">No notifications right now</p>
+            <p>We’ll let you know when there’s an update.</p>
+          </div>
+        </div>
+      )
+    }}
+    renderItem={item => {
+      return (
+        <div className="flex items-start">
+          <ExclamationCircleOutlined className="mr-3 text-red-500 text-xl" />
+          <div className="inline-block mt-1">
+            <div>
+              <span className="font-bold">
+                {item.first_name + ' ' + item.last_name + `'s `}
+              </span>
+              {'subsidy authorization expires on ' +
+                dayjs(item.effective_on.slice(5, 16), 'DD MMM YYYY').format(
+                  'MMM D'
+                ) +
+                '. ' +
+                'Email the updated letter to '}
+              <a
+                className="underline"
+                href={`mailto:${PIE_FOR_PROVIDERS_EMAIL}`}
+              >
+                {PIE_FOR_PROVIDERS_EMAIL + '.'}
+              </a>
+            </div>
+            <div className="mt-1 text-gray-400">
+              {dayjs(item.expiration_date.slice(5, 16), 'DD MMM YYYY').format(
+                'MMM D, YYYY'
+              ) || ''}
+            </div>
+            <Divider />
+          </div>
+        </div>
+      )
+    }}
+  />
 )
 
 Notifications.propTypes = {
