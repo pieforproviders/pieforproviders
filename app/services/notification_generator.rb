f96f145bc255f0029c012f525d6949d8e0b923f2
OliverCai0 <47547771+OliverCai0@users.noreply.github.com>
2022-06-14 14:02:25 -0400

    small fix to notif generator

--- app/services/notification_generator.rb
+++ app/services/notification_generator.rb
@@ -15,8 +15,7 @@ class NotificationGenerator
   end
 
   def generate_notifications
-    approvals_without_notification = Approval.where.missing(:notifications)\
-                                             .where('expires_on between ? and ?', 0.days.after, 30.days.after)
+    approvals_without_notification = Approval.where.missing(:notifications).where(expires_on: 0.days.after..30.days.after)
     return unless approvals_without_notification.length.positive?
 
     approvals_without_notification.each do |approval|
