3dff24d4b3f82810a1b87e4d9c6bd71a0dbd2fd8
OliverCai0 <47547771+OliverCai0@users.noreply.github.com>
2022-06-28 16:38:43 -0400

    trying to get rubocop db to pass

--- db/schema.rb
+++ db/schema.rb
@@ -87,9 +87,9 @@ ActiveRecord::Schema.define(version: 2022_06_21_165259) do
     t.decimal "special_needs_daily_rate"
     t.decimal "special_needs_hourly_rate"
     t.boolean "enrolled_in_school"
-    t.decimal "authorized_weekly_hours", precision: 5, scale: 2
     t.string "rate_type"
     t.uuid "rate_id"
+    t.decimal "authorized_weekly_hours", precision: 5, scale: 2
     t.date "deleted_at"
     t.index ["approval_id"], name: "index_child_approvals_on_approval_id"
     t.index ["child_id"], name: "index_child_approvals_on_child_id"
@@ -193,7 +193,7 @@ ActiveRecord::Schema.define(version: 2022_06_21_165259) do
   end
 
   create_table "nebraska_dashboard_cases", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "month", default: "2022-05-05 01:37:11", null: false
+    t.datetime "month", default: "2022-06-28 20:30:26", null: false
     t.string "attendance_risk", default: "not_enough_info", null: false
     t.integer "absences", default: 0, null: false
     t.integer "earned_revenue_cents"
@@ -270,6 +270,7 @@ ActiveRecord::Schema.define(version: 2022_06_21_165259) do
     t.string "earned_revenue_currency", default: "USD", null: false
     t.uuid "schedule_id"
     t.string "absence_type"
+    t.index ["child_id", "date"], name: "index_service_days_on_child_id_and_date", unique: true
     t.index ["child_id"], name: "index_service_days_on_child_id"
     t.index ["date"], name: "index_service_days_on_date"
     t.index ["schedule_id"], name: "index_service_days_on_schedule_id"
