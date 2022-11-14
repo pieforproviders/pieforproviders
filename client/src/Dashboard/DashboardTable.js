1182e267e964db15b8d7bf6a7037310b5ea72af5
Martin <murriaguimartin@gmail.com>
2022-11-14 16:04:25 -0500

    martin local settings

--- client/src/Dashboard/DashboardTable.js
+++ client/src/Dashboard/DashboardTable.js
@@ -173,7 +173,7 @@ export default function DashboardTable({
       .map(({ name = '', children = [], ...options }) => {
         const hasDefinition = [
           'attendance',
-          // 'revenue',
+          // // 'revenue',
           'totalAuthorizationPeriod'
         ]
         return {
@@ -435,18 +435,17 @@ export default function DashboardTable({
         sorter: (a, b) => a.attendanceRate.rate - b.attendanceRate.rate,
         render: renderAttendanceRate
       },
-      // {
-      //   name: 'guaranteedRevenue',
-      //   sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue,
-      //   render: renderDollarAmount
-      // },
-      // {
-      //   name: 'potentialRevenue',
-      //   sorter: (a, b) => a.potentialRevenue - b.potentialRevenue,
-      //   render: renderDollarAmount
-      // },
-
-      {
+      // // {
+      // //   name: 'guaranteedRevenue',
+      // //   sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue,
+      // //   render: renderDollarAmount
+      // // },
+      // // {
+      // //   name: 'potentialRevenue',
+      // //   sorter: (a, b) => a.potentialRevenue - b.potentialRevenue,
+      // //   render: renderDollarAmount
+      // // },
+       {
         name: 'maxApprovedRevenue',
         sorter: (a, b) => a.maxApprovedRevenue - b.maxApprovedRevenue,
         render: renderDollarAmount
