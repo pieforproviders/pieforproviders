7e386669bc26e80930b400ac9e23ceacdcba0c1a
Martin <murriaguimartin@gmail.com>
2022-11-14 16:04:25 -0500

    martin local settings

--- client/src/Dashboard/__tests__/DashboardTable.test.js
+++ client/src/Dashboard/__tests__/DashboardTable.test.js
@@ -22,12 +22,11 @@ describe('<DashboardTable />', () => {
   it('renders the DashboardTable component', async () => {
     const { container } = doRender()
     await waitFor(() => {
-      expect(screen.getAllByRole('columnheader').length).toEqual(9)
+      expect(screen.getAllByRole('columnheader').length).toEqual(8)
       expect(container).toHaveTextContent('Child name')
       expect(container).toHaveTextContent('Case number')
       expect(container).toHaveTextContent('Attendance rate')
-      expect(container).not.not.toHaveTextContent('Earned revenue')
-      expect(container).not.toHaveTextContent('Estimated revenue')
+      expect(container).not.toHaveTextContent('Earned revenue')
       expect(container).not.toHaveTextContent('Estimated revenue')
       expect(container).toHaveTextContent('Max. approved revenue')
       expect(container).toHaveTextContent('Authorized period')
@@ -50,6 +49,8 @@ describe('<DashboardTable />', () => {
       expect(container).toHaveTextContent('Absences')
       expect(container).toHaveTextContent('Earned revenue')
       expect(container).toHaveTextContent('Estimated revenue')
+      // expect(container).not.toHaveTextContent('Earned revenue')
+      // expect(container).not.toHaveTextContent('Estimated revenue')
       expect(container).toHaveTextContent('Family fee')
       expect(container).toHaveTextContent('Authorized period')
       expect(container).toHaveTextContent('Total hours remaining')
