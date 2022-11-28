29de91312ab6a7b184b5968a04c26b70e2019722
Martin <murriaguimartin@gmail.com>
2022-11-28 14:04:15 -0500

    [PIE-2902]UI updates to prepare for Release 1

--- client/src/Dashboard/DashboardTable.js
+++ client/src/Dashboard/DashboardTable.js
@@ -435,36 +435,21 @@ export default function DashboardTable({
         sorter: (a, b) => a.attendanceRate.rate - b.attendanceRate.rate,
         render: renderAttendanceRate
       },
-      // // {
-      // //   name: 'guaranteedRevenue',
-      // //   sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue,
-      // //   render: renderDollarAmount
-      // // },
-      // // {
-      // //   name: 'potentialRevenue',
-      // //   sorter: (a, b) => a.potentialRevenue - b.potentialRevenue,
-      // //   render: renderDollarAmount
-      // // },
- 
+      // {
+      //   name: 'guaranteedRevenue',
+      //   sorter: (a, b) => a.guaranteedRevenue - b.guaranteedRevenue,
+      //   render: renderDollarAmount
+      // },
+      // {
+      //   name: 'potentialRevenue',
+      //   sorter: (a, b) => a.potentialRevenue - b.potentialRevenue,
+      //   render: renderDollarAmount
+      // },
+
       {
         name: 'maxApprovedRevenue',
         sorter: (a, b) => a.maxApprovedRevenue - b.maxApprovedRevenue,
         render: renderDollarAmount
-      },
-      {
-        name: 'authorizedPeriod',
-        sorter: (a, b) =>
-          dayjs(a.approvalEffectiveOn) - dayjs(b.approvalEffectiveOn),
-        render: (text, record) =>
-          isInactive(record)
-            ? '-'
-            : isNotApproved(record)
-            ? 'unknown'
-            : `${dayjs(record.approvalEffectiveOn).format('M/D/YY')}${
-                record.approvalExpiresOn
-                  ? ` - ${dayjs(record.approvalExpiresOn).format('M/D/YY')}`
-                  : ''
-              }`
       },
       {
         name: 'authorizedPeriod',
--- client/src/Dashboard/__tests__/DashboardTable.test.js
+++ client/src/Dashboard/__tests__/DashboardTable.test.js
@@ -22,7 +22,7 @@ describe('<DashboardTable />', () => {
   it('renders the DashboardTable component', async () => {
     const { container } = doRender()
     await waitFor(() => {
-      expect(screen.getAllByRole('columnheader').length).toEqual(8)
+      expect(screen.getAllByRole('columnheader').length).toEqual(9)
       expect(container).toHaveTextContent('Child name')
       expect(container).toHaveTextContent('Case number')
       expect(container).toHaveTextContent('Attendance rate')
@@ -49,8 +49,6 @@ describe('<DashboardTable />', () => {
       expect(container).toHaveTextContent('Absences')
       expect(container).toHaveTextContent('Earned revenue')
       expect(container).toHaveTextContent('Estimated revenue')
-      // expect(container).not.toHaveTextContent('Earned revenue')
-      // expect(container).not.toHaveTextContent('Estimated revenue')
       expect(container).toHaveTextContent('Family fee')
       expect(container).toHaveTextContent('Authorized period')
       expect(container).toHaveTextContent('Total hours remaining')
