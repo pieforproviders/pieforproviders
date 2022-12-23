1b604cd80ebb5e7786211759f09cac6625e5e725
Martin <murriaguimartin@gmail.com>
2022-12-22 23:38:30 -0500

    attendance risk calculator adjusments && test for the Illinois attendance risk calculator

--- spec/services/illinois_attendance_risk_calculator_spec.rb
+++ spec/services/illinois_attendance_risk_calculator_spec.rb
@@ -3,24 +3,136 @@
 require 'rails_helper'
 
 RSpec.describe IllinoisAttendanceRiskCalculator, type: :service do
-  describe '#elapsed_eligible_days' do 
+  describe '#elapsed_eligible_days' do
     it 'calculate elapsed eligible days for child with attended info' do
       business = create(:business)
       child = create(:child_in_illinois, business: business)
       date = Date.new(2022, 12, 20)
       elapsed_days = 19
       weekend_days = 6
-      # binding.pry
+      elapsed_eligible_days = elapsed_days - weekend_days
       risk_calculator_elapsed_days = described_class.new(child, date).send(:elapsed_eligible_days)
-      expect(risk_calculator_elapsed_days).to eq(elapsed_days - weekend_days)
+      expect(risk_calculator_elapsed_days).to eq(elapsed_eligible_days)
     end
 
     it 'calculate elapsed eligible days when business has closures' do
-      business = create(:business_with_closed_days_in_november)
+      date = Date.new(2022, 12, 20)
+      business = create(:business)
+      create(:business_closure, business: business, date: date)
       child = create(:child_in_illinois, business: business)
-      date = Date.new(2022, 11, 22)
+      elapsed_days = 19
+      closed_days = 7
+      elapsed_eligible_days = elapsed_days - closed_days
       risk_calculator_elapsed_days = described_class.new(child, date).send(:elapsed_eligible_days)
-      binding.pry
+      expect(risk_calculator_elapsed_days).to eq(elapsed_eligible_days)
+    end
+  end
+
+  describe '#attended_days' do
+    it 'calculate attended days until given date' do
+      business = create(:business)
+      child = create(:child_in_illinois, business: business)
+      date = Time.current
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child)
+      service_part_day = create(:service_day, child: child)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child.active_child_approval(attendance_date)
+      )
+      # create(
+      #   :illinois_part_day_attendance,
+      #   service_day: service_part_day,
+      #   child_approval: child.active_child_approval(Time.current)
+      # )
+
+      perform_enqueued_jobs
+
+      full_days = child.service_days.for_month(date).map(&:full_time).compact.reduce(:+) || 0
+      part_days = child.service_days.for_month(date).map(&:part_time).compact.reduce(:+) || 0
+      total_days = full_days + part_days
+      attendance_service = described_class.new(child, date)
+
+      expect(attendance_service.send(:attended_days)).to eq(total_days)
+    end
+  end
+
+  describe '#attendance_rate_until_date' do
+    it 'caculate attendance rate until given date' do
+      business = create(:business)
+      child = create(:child_in_illinois, business: business)
+      date = Time.current
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child.active_child_approval(attendance_date)
+      )
+      elapsed_days = date - Time.current.at_beginning_of_month
+      full_days = child.service_days.for_month(date).map(&:full_time).compact.reduce(:+) || 0
+      part_days = child.service_days.for_month(date).map(&:part_time).compact.reduce(:+) || 0
+      total_attended_days = full_days + part_days
+      attendance_service = described_class.new(child, date)
+
+      expect(attendance_service.send(:attendance_rate_until_date)).to eq(total_attended_days.to_f / elapsed_days)
+    end
+  end
+
+  describe '#risk_label' do
+    it 'return not_enough_info label when there is no attendances' do
+      business = create(:business)
+      child = create(:child_in_illinois, business: business)
+      date = Date.new(2022, 12, 20)
+      attendance_rate_until_date = described_class.new(child, date).call
+      expect(attendance_rate_until_date).to eq('not_enough_info')
+    end
+
+    it 'return not_enough_info label with attendance info but less than halfway through month' do
+      # travel_to Faker::Time.between(from: Time.current.at_beginning_of_year, to: Time.current)
+      travel_to Time.current.at_beginning_of_month + rand(1..14).days
+      business = create(:business)
+      child = create(:child_in_illinois, business: business)
+      date = Time.current.at_beginning_of_month + 5.days
+
+      3.times do |idx|
+        service_day = create(:service_day,
+                             date: date.in_time_zone(Child.first.timezone).prev_occurring(:monday) + idx.days,
+                             child: child)
+        create(:illinois_part_day_attendance,
+               service_day: service_day,
+               child_approval: child.child_approvals.first,
+               check_in: service_day.date + 3.hours)
+      end
+
+      perform_enqueued_jobs
+
+      attendance_service = described_class.new(child, Time.current)
+
+      expect(attendance_service.call).to eq('not_enough_info')
+    end
+
+    it 'return at_risk label when attended rate is below treshold' do
+      travel_to Time.current.at_beginning_of_month + rand(15..28).days
+      business = create(:business)
+      child = create(:child_in_illinois, business: business)
+      date = Time.current.at_beginning_of_month
+
+      7.times do |idx|
+        service_day = create(:service_day,
+                             date: date.in_time_zone(Child.first.timezone) + idx.days,
+                             child: child)
+        create(:illinois_part_day_attendance,
+               service_day: service_day,
+               child_approval: child.child_approvals.first,
+               check_in: service_day.date + 3.hours)
+      end
+
+      perform_enqueued_jobs
+
+      attendance_service = described_class.new(child, Time.current)
+      expect(attendance_service.call).to eq('at_risk')
     end
   end
 end
