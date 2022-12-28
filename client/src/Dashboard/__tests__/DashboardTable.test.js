07f4b3057b45820b5210ab86b870a31905b44681
Martin <murriaguimartin@gmail.com>
2022-12-28 12:43:37 -0500

    [PIE-2937][Martin] Frontend tests fixed

--- client/src/Dashboard/__tests__/DashboardTable.test.js
+++ client/src/Dashboard/__tests__/DashboardTable.test.js
@@ -22,9 +22,9 @@ describe('<DashboardTable />', () => {
   it('renders the DashboardTable component', async () => {
     const { container } = doRender()
     await waitFor(() => {
-      expect(screen.getAllByRole('columnheader').length).toEqual(9)
-      expect(container).toHaveTextContent('Child name')
-      expect(container).toHaveTextContent('Case number')
+      expect(screen.getAllByRole('columnheader').length).toEqual(7)
+      expect(container).toHaveTextContent('Child')
+      expect(container).not.toHaveTextContent('Case number')
       expect(container).toHaveTextContent('Attendance rate')
       expect(container).toHaveTextContent('Earned revenue')
       expect(container).not.toHaveTextContent('Estimated revenue')
