23297cff649633a0deac493f0b74a126550865af
Martin <murriaguimartin@gmail.com>
2023-01-10 11:46:06 -0500

    Quality rating bump for IL

--- spec/models/illinois/dashboard_case_spec.rb
+++ spec/models/illinois/dashboard_case_spec.rb
@@ -25,5 +25,101 @@ RSpec.describe Illinois::DashboardCase do
       ).guaranteed_revenue)
         .to eq(0)
     end
+
+    it 'returns guaranteed revenue for business without quality rating' do
+      fcc_business = create(:business, license_type: 'family_child_care_home_i', quality_rating: nil)
+      child_from_fcc = create(:child_in_illinois, business: fcc_business)
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child_from_fcc)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child_from_fcc.active_child_approval(attendance_date)
+      )
+
+      perform_enqueued_jobs
+
+      revenues = described_class.new(child: child_from_fcc, filter_date: Time.current)
+
+      earned_revenue = revenues.earned_revenue_below_threshold
+
+      quality_bump = 1
+
+      # binding.pry
+
+      expect(revenues.guaranteed_revenue).to eq(earned_revenue * quality_bump)
+    end
+
+    it 'returns guaranteed revenue for business with bronze quality rating' do
+      fcc_business = create(:business, license_type: 'family_child_care_home_i', quality_rating: 'bronze')
+      child_from_fcc = create(:child_in_illinois, business: fcc_business)
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child_from_fcc)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child_from_fcc.active_child_approval(attendance_date)
+      )
+
+      perform_enqueued_jobs
+
+      revenues = described_class.new(child: child_from_fcc, filter_date: Time.current)
+
+      earned_revenue = revenues.earned_revenue_below_threshold
+
+      quality_bump = 1
+
+      # binding.pry
+
+      expect(revenues.guaranteed_revenue).to eq(earned_revenue * quality_bump)
+    end
+
+    it 'returns guaranteed revenue for business with silver quality rating' do
+      fcc_business = create(:business, license_type: 'family_child_care_home_i', quality_rating: 'silver')
+      child_from_fcc = create(:child_in_illinois, business: fcc_business)
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child_from_fcc)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child_from_fcc.active_child_approval(attendance_date)
+      )
+
+      perform_enqueued_jobs
+
+      revenues = described_class.new(child: child_from_fcc, filter_date: Time.current)
+
+      earned_revenue = revenues.earned_revenue_below_threshold
+
+      quality_bump = 1
+
+      # binding.pry
+
+      expect(revenues.guaranteed_revenue).to eq(earned_revenue * quality_bump)
+    end
+
+    it 'returns guaranteed revenue for business with gold quality rating' do
+      fcc_business = create(:business, license_type: 'family_child_care_home_i', quality_rating: 'gold')
+      child_from_fcc = create(:child_in_illinois, business: fcc_business)
+      attendance_date = Time.current.at_beginning_of_month
+      service_full_day = create(:service_day, child: child_from_fcc)
+      create(
+        :illinois_full_day_attendance,
+        service_day: service_full_day,
+        child_approval: child_from_fcc.active_child_approval(attendance_date)
+      )
+
+      perform_enqueued_jobs
+
+      revenues = described_class.new(child: child_from_fcc, filter_date: Time.current)
+
+      earned_revenue = revenues.earned_revenue_below_threshold
+
+      quality_bump = 1
+
+      # binding.pry
+
+      expect(revenues.guaranteed_revenue).to eq(earned_revenue * quality_bump)
+    end
   end
 end
