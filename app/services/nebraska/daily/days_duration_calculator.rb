d5d93186ebbb0d81c2e050a583a3edc3cce1b2b1
Martín Murriagui <murriaguimartin@gmail.com>
2023-07-28 11:00:10 -0500

    fix rubocop fails

--- app/services/nebraska/daily/days_duration_calculator.rb
+++ app/services/nebraska/daily/days_duration_calculator.rb
@@ -4,10 +4,9 @@ module Nebraska
   module Daily
     # Calculate full days attended for a given service day
     class DaysDurationCalculator
-      attr_reader :total_time_in_care
-      attr_reader :filter_date
+      attr_reader :total_time_in_care, :filter_date
 
-      def initialize(total_time_in_care:, filter_date: Date.today)
+      def initialize(total_time_in_care:, filter_date: Time.zone.today)
         @total_time_in_care = total_time_in_care
         @filter_date = filter_date
       end
@@ -19,7 +18,7 @@ module Nebraska
       private
 
       def calculate_days
-        control_date = Date.new(2023,7,1)
+        control_date = Date.new(2023, 7, 1)
         return total_time_in_care > 5.hours + 45.minutes ? 1 : 0 if filter_date < control_date
         return total_time_in_care >= 5.hours ? 1 : 0 if filter_date >= control_date
       end
