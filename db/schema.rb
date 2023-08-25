a42f937c76998a41cfde6eeb651c4b6f101ae300
Martín Murriagui <43682273+MartinM1312@users.noreply.github.com>
2023-08-25 08:21:53 -0500

    Pie 3324 (#3339)
    * feat: [PIE-3318] [Fabian] Spike for matching names
    
    * [PIE-3323][Martin] Names Matching engine service for finding children on the DB
    
    * [PIE-3324][Martin] Update CSV PDF and XLSX uploaders with the new Matching Engine / Updated error messages
    
    * feat: [PIE-3318] [Fabian] Spike for matching names
    
    * [PIE-3324][Martin] Update CSV PDF and XLSX uploaders with the new Matching Engine / Updated error messages
    
    * [PIE-3324][Martin] Csv importer finds child by dhs_id when name missing
    
    * remove unnecesary rake
    
    ---------
    
    Co-authored-by: Fabian Aguirre <fabianraf@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2023_07_04_230941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
+  enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
