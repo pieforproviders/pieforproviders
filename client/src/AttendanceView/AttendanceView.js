8851fe0a8fa6058ccff94e3f528a4db08854621f
Martín Murriagui <murriaguimartin@gmail.com>
2024-02-26 08:28:02 -0500

    Remove wondershcool id logic on attendace edit

--- client/src/AttendanceView/AttendanceView.js
+++ client/src/AttendanceView/AttendanceView.js
@@ -179,17 +179,6 @@ export function AttendanceView() {
             )
           })
 
-          const hasWoderschoolId = () => {
-            if (
-              matchingServiceDay?.child?.wonderschool_id == null ||
-              matchingServiceDay?.child?.wonderschool_id === undefined ||
-              matchingServiceDay?.child?.wonderschool_id.toLowerCase() === 'no'
-            ) {
-              return false
-            } else {
-              return true
-            }
-          }
 
           const handleEditAttendance = () => {
             const serviceDay = record.serviceDays.find(
@@ -236,14 +225,14 @@ export function AttendanceView() {
             if (matchingServiceDay.tags.includes('absence')) {
               return (
                 <div>
-                  {hasWoderschoolId() ? null : (
+                  {
                     <button
                       className="float-right edit-icon"
                       onClick={handleEditAttendance}
                     >
                       <EditIcon />
                     </button>
-                  )}
+                  }
                   <div className="flex justify-center">
                     <div
                       className="box-border p-1 bg-orange2 text-orange3"
@@ -331,14 +320,14 @@ export function AttendanceView() {
 
             return (
               <div className="relative text-center body-2">
-                {hasWoderschoolId() ? null : (
+                {
                   <button
                     onClick={handleEditAttendance}
                     className="absolute right-0 rounded-full group hover:bg-blue3 focus:bg-primaryBlue"
                   >
                     <EditIcon className="m-1.5 fill-gray3 group-hover:fill-primaryBlue group-focus:fill-white" />
                   </button>
-                )}
+                }
                 <div className="mb-2 text-gray8 font-semiBold">
                   {totalTimeInCare}
                 </div>
