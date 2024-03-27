8c7dc693bc9163f83be5169d740e772faf3c52b7
Martín Murriagui <murriaguimartin@gmail.com>
2024-03-27 10:22:37 -0500

    upgrade on name matching engine

--- app/services/name_matching_actions.rb
+++ app/services/name_matching_actions.rb
@@ -2,9 +2,8 @@
 
 # Actions for name matching engine results
 class NameMatchingActions
-  def initialize(match_tag:, match_child:, file_child:, business:)
-    @match_tag = match_tag
-    @match_child = match_child
+  def initialize(match_children:, file_child:, business:)
+    @match_children = match_children
     @file_child = file_child
     @business = business
   end
@@ -19,36 +18,39 @@ class NameMatchingActions
   # rubocop:disable Metrics/MethodLength
   def user_outputs
     # rubocop:disable Rails/Output
-    case @match_tag
-    when 'exact_match'
-      @business.children.find_by(
-        first_name: @file_child[0], last_name: @file_child[1]
-      )
-    when 'close_match'
-      rows = []
-      puts Rainbow("This child doesn't exactly match P4P's child records").yellow
+    @match_children.each do |match_child|
+      match_tag = match_child[:match_tag]
+      case match_tag
+      when 'exact_match'
+        return @business.children.find_by(
+          first_name: @file_child[0], last_name: @file_child[1]
+        )
+      when 'close_match'
+        rows = []
+        puts Rainbow("This child doesn't exactly match P4P's child records").yellow
 
-      rows << [Rainbow('We found: ').bright + Rainbow("#{@file_child[0]} #{@file_child[1]}").yellow]
-      rows << [Rainbow('P4P has: ').bright +
-               Rainbow("#{@match_child['first_name']} #{@match_child['last_name']}").yellow]
+        rows << [Rainbow('We found: ').bright + Rainbow("#{@file_child[0]} #{@file_child[1]}").yellow]
+        rows << [Rainbow('P4P has: ').bright +
+                 Rainbow("#{match_child['first_name']} #{match_child['last_name']}").yellow]
 
-      table = Terminal::Table.new(rows:)
-      puts table
+        table = Terminal::Table.new(rows:)
+        puts table
 
-      $stdout.puts Rainbow('Is this the same child? Yes or No').yellow
-      input = $stdin.gets.chomp
+        $stdout.puts Rainbow('Is this the same child? Yes or No').yellow
+        input = $stdin.gets.chomp
 
-      if input.downcase == 'yes'
-        puts Rainbow("Thanks, we'll upload this attendance!").green
-        @business.children.find_by(first_name: @match_child['first_name'], last_name: @match_child['last_name'])
-      else
-        puts Rainbow("Okay, we'll skip over this attendance.").yellow
+        if input.downcase == 'yes'
+          puts Rainbow("Thanks, we'll upload this attendance!").green
+          return @business.children.find_by(first_name: match_child['first_name'], last_name: match_child['last_name'])
+        else
+          puts Rainbow("Okay, we'll skip over this record.").yellow
+          nil
+        end
+
+      when 'no_match'
+        puts Rainbow("There's no match for #{@file_child[0]} #{@file_child[1]}. Please check the file").red
         nil
       end
-
-    when 'no_match'
-      puts Rainbow("There's no match for #{@file_child[0]} #{@file_child[1]} on the system. Please check the file").red
-      nil
     end
   end
   # rubocop:enable Rails/Output
